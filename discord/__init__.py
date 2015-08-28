5db2b31114c4ddb16e05c54c4d3f6874593fd416
Rapptz <rapptz@gmail.com>
2015-08-27 22:15:01 -0400

    Version bump to v0.4.0.

--- discord/__init__.py
+++ discord/__init__.py
@@ -15,8 +15,8 @@ __title__ = 'discord'
 __author__ = 'Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Rapptz'
-__version__ = '0.3.1'
-__build__ = 0x003010
+__version__ = '0.4.0'
+__build__ = 0x004000
 
 from client import Client
 from user import User
