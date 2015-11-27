af9e5bc67a189096663a694cd99484e14877d1d7
Rapptz <rapptz@gmail.com>
2015-11-26 19:30:09 -0500

    Simplify examples to no longer use logging or Client.is_logged_in
    The logging module is no longer required to get diagnostic output
    since we use `stderr` by default regardless of the logging module.
    Which means that the logging module will only give a more verbose
    output than is necessary.
    
    Client.is_logged_in checking is no longer necessary since all HTTP
    request handling now raise an exception if they fail so those chunks
    are also gone.

--- examples/deleted.py
+++ examples/deleted.py
@@ -1,15 +1,7 @@
 import discord
-import logging
-
-# Set up the logging module to output diagnostic to the console.
-logging.basicConfig()
 
 client = discord.Client()
 client.login('email', 'password')
-
-if not client.is_logged_in:
-    print('Logging in to Discord failed')
-    exit(1)
 
 @client.event
 def on_ready():
--- examples/edits.py
+++ examples/edits.py
@@ -1,16 +1,8 @@
 import discord
 import time
-import logging
-
-# Set up the logging module to output diagnostic to the console.
-logging.basicConfig()
 
 client = discord.Client()
 client.login('email', 'password')
-
-if not client.is_logged_in:
-    print('Logging in to Discord failed')
-    exit(1)
 
 @client.event
 def on_ready():
