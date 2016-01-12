bbe6455eb92132a3eab3be5a1d135e6392c17136
Khazhismel <khazhy@gmail.com>
2016-01-12 11:49:22 -0500

    Guild member add will include deaf and mute now, do not provide default values

--- discord/state.py
+++ discord/state.py
@@ -194,7 +194,7 @@ class ConnectionState:
 
     def parse_guild_member_add(self, data):
         server = self._get_server(data.get('guild_id'))
-        member = Member(server=server, deaf=False, mute=False, **data)
+        member = Member(server=server, **data)
         member.roles.append(server.default_role)
         server._add_member(member)
         self.dispatch('member_join', member)
