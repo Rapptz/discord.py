5b827ebbb185808885cb1b86c64bc1bac64aaed4
Sérgio Prata <mephx.x@gmail.com>
2016-04-05 21:03:07 +0100

    added proper handling of my_background_task future

--- examples/background_task.py
+++ examples/background_task.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+from asyncio.futures import CancelledError
 
 client = discord.Client()
 
@@ -7,10 +8,13 @@ async def my_background_task():
     await client.wait_until_ready()
     counter = 0
     channel = discord.Object(id='channel_id_here')
-    while not client.is_closed:
-        counter += 1
-        await client.send_message(channel, counter)
-        await asyncio.sleep(60) # task runs every 60 seconds
+    try:
+        while not client.is_closed:
+            counter += 1
+            await client.send_message(channel, counter)
+            await asyncio.sleep(60) # task runs every 60 seconds
+    except CancelledError:
+        pass
 
 @client.event
 async def on_ready():
@@ -22,10 +26,19 @@ async def on_ready():
 loop = asyncio.get_event_loop()
 
 try:
-    loop.create_task(my_background_task())
+    my_background_task_future = loop.create_task(my_background_task())
     loop.run_until_complete(client.login('email', 'password'))
     loop.run_until_complete(client.connect())
 except Exception:
+    my_background_task_future.cancel()
     loop.run_until_complete(client.close())
+    pending = asyncio.Task.all_tasks()
+    gathered = asyncio.gather(*pending)
+    try:
+        gathered.cancel()
+        loop.run_forever()
+        gathered.exception()
+    except:
+        pass
 finally:
     loop.close()
