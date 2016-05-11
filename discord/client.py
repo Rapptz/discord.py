132873d1591452b71b006417522778a0b96b4fea
Twentysix <Twentysix26@users.noreply.github.com>
2016-05-11 17:13:34 +0200

    Add clarification for purge_from and delete_messages
    Endpoint not usable by non-bot accounts

--- discord/client.py
+++ discord/client.py
@@ -992,6 +992,8 @@ class Client:
         channel is not consistent throughout the entire sequence, then an
         :exc:`HTTPException` will be raised.
 
+        Usable only by bot accounts.
+
         Parameters
         -----------
         messages : iterable of :class:`Message`
@@ -1002,7 +1004,8 @@ class Client:
         ClientException
             The number of messages to delete is less than 2 or more than 100.
         Forbidden
-            You do not have proper permissions to delete the messages.
+            You do not have proper permissions to delete the messages or 
+            you're not using a bot account.
         HTTPException
             Deleting the messages failed.
         """
@@ -1034,6 +1037,8 @@ class Client:
         your own. The Read Message History permission is also needed to retrieve
         message history.
 
+        Usable only by bot accounts.
+
         Parameters
         -----------
         channel : :class:`Channel`
@@ -1052,7 +1057,8 @@ class Client:
         Raises
         -------
         Forbidden
-            You do not have proper permissions to do the actions required.
+            You do not have proper permissions to do the actions required or
+            you're not using a bot account.
         HTTPException
             Purging the messages failed.
 
