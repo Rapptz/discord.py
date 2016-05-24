921153a1f259aa80491668f1aec0372999458db6
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-05-23 20:37:14 -0500

    add parameter to `process_commands()` to allow it to run if author is bot user
    Like the title says.
    Adds a parameter (default to False) which lets a user allow process_commands() to trigger even if the message author is the bot user.

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -551,7 +551,7 @@ class Bot(GroupMixin, discord.Client):
     # command processing
 
     @asyncio.coroutine
-    def process_commands(self, message):
+    def process_commands(self, message, allow = False):
         """|coro|
 
         This function processes the commands that have been registered
@@ -578,8 +578,9 @@ class Bot(GroupMixin, discord.Client):
         _internal_author = message.author
 
         view = StringView(message.content)
-        if message.author == self.user:
-            return
+        if not allow:
+            if message.author == self.user:
+                return
 
         prefix = self._get_prefix(message)
         invoked_prefix = prefix
