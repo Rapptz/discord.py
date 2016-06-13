e8c32c542eb4a816db44a94c8af6578f4e9cb9e3
Rapptz <rapptz@gmail.com>
2016-06-12 20:36:07 -0400

    Make HTTPException get the error JSON's message attribute.

--- discord/errors.py
+++ discord/errors.py
@@ -62,13 +62,17 @@ class HTTPException(DiscordException):
 
     def __init__(self, response, message):
         self.response = response
-        self.text = message
+        if type(message) is dict:
+            self.text = message.get('message', '')
+            self.code = message.get('code', 0)
+        else:
+            self.text = message
 
         fmt = '{0.reason} (status code: {0.status})'
-        if len(message) < 100:
+        if len(self.text):
             fmt = fmt + ': {1}'
 
-        super().__init__(fmt.format(self.response, message))
+        super().__init__(fmt.format(self.response, self.text))
 
 class Forbidden(HTTPException):
     """Exception that's thrown for when status code 403 occurs.
