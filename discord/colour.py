07607060c1ce07c613552a0233d53a1e821a0c31
Rapptz <rapptz@gmail.com>
2016-06-16 22:49:34 -0400

    Fix Colour.__str__ to actually pad zeroes.

--- discord/colour.py
+++ discord/colour.py
@@ -65,7 +65,7 @@ class Colour:
         return not self.__eq__(other)
 
     def __str__(self):
-        return '#' + format(self.value, 'x')
+        return '#{:0>6x}'.format(self.value)
 
     def __hash__(self):
         return hash(self.value)
