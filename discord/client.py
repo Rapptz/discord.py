fbd628fae3e82fb9d10fce5c0e090a3ac5e45b13
Rapptz <rapptz@gmail.com>
2016-07-25 06:16:22 -0400

    Fix KeyError when creating private channels in start_private_message.
    I was not passing the full updated v6 payload.

--- discord/client.py
+++ discord/client.py
@@ -767,7 +767,7 @@ class Client:
             raise InvalidArgument('user argument must be a User')
 
         data = yield from self.http.start_private_message(user.id)
-        channel = PrivateChannel(me=self.user, id=data['id'], user=user)
+        channel = PrivateChannel(me=self.user, **data)
         self.connection._add_private_channel(channel)
         return channel
 
