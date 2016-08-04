6344504dfce8036e0c126aeac0a9f2865b99f0e3
Khazhismel <khazhy@gmail.com>
2016-08-04 00:54:18 -0700

    Emoji.url uses api url.

--- discord/emoji.py
+++ discord/emoji.py
@@ -104,4 +104,4 @@ class Emoji(Hashable):
     @property
     def url(self):
         """Returns a URL version of the emoji."""
-        return "https://discordcdn.com/emojis/{0.id}.png".format(self)
+        return "https://discordapp.com/api/emojis/{0.id}.png".format(self)
