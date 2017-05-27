ee3c77353feb3bc4a4f4712e0b7581c9184be2d9
Rapptz <rapptz@gmail.com>
2017-05-27 01:12:06 -0400

    Add highest verification level.

--- discord/enums.py
+++ discord/enums.py
@@ -70,11 +70,13 @@ class VoiceRegion(Enum):
         return self.value
 
 class VerificationLevel(Enum):
-    none       = 0
-    low        = 1
-    medium     = 2
-    high       = 3
-    table_flip = 3
+    none              = 0
+    low               = 1
+    medium            = 2
+    high              = 3
+    table_flip        = 3
+    highest           = 4
+    double_table_flip = 4
 
     def __str__(self):
         return self.name
