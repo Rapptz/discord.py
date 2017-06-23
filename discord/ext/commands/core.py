7fc5d371f4944545748dff76cc32cd03662337f6
Jack Lin <jacklin213@gmail.com>
2017-06-23 15:05:58 +1200

    Update MissingRequiredArgument exception message
    Changed message from "is a required argument that is missing" to "has a required argument that is missing"

--- discord/ext/commands/core.py
+++ discord/ext/commands/core.py
@@ -209,7 +209,7 @@ class Command:
             if param.kind == param.VAR_POSITIONAL:
                 raise RuntimeError() # break the loop
             if required:
-                raise MissingRequiredArgument('{0.name} is a required argument that is missing.'.format(param))
+                raise MissingRequiredArgument('{0.name} has a required argument that is missing.'.format(param))
             return param.default
 
         if consume_rest_is_special:
