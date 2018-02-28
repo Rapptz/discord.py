6524068087dea715afadbbc7b5887f626db83cb2
daegontaven <daegontaven@gmail.com>
2018-02-28 03:56:59 -0500

    Add method to search for cogs required by load_extension.

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -25,18 +25,20 @@ DEALINGS IN THE SOFTWARE.
 """
 
 import asyncio
-import discord
-import inspect
 import importlib
+import inspect
+import pkgutil
+import re
 import sys
 import traceback
-import re
 
-from .core import GroupMixin, Command, command
-from .view import StringView
+import discord
 from .context import Context
+from .core import GroupMixin, Command
 from .errors import CommandNotFound, CommandError
 from .formatter import HelpFormatter
+from .view import StringView
+
 
 def when_mentioned(bot, msg):
     """A callable that implements a command prefix equivalent to being mentioned.
@@ -569,6 +571,52 @@ class BotBase(GroupMixin):
             # register event listeners the cog has
             if name.startswith('on_'):
                 self.add_listener(member, name)
+
+    @staticmethod
+    def find_cogs(package):
+        """Finds all modules in package and presents them in the
+        format required by :func: `load_extension`.
+
+        This is useful when you need to load cogs from multiple
+        areas of your bot. Simply convert your cogs directory
+        into a package and run this method on it.
+
+        Parameters
+        -----------
+        package : package
+            Your cogs directory as a python package.
+
+        Returns
+        --------
+        list or None
+            A list of strings of format `foo.bar` as required by
+            :func: `load_extension`. If package passed is not
+            valid then `None` is returned instead.
+        """
+        loader = pkgutil.get_loader(package)
+        if loader is None:
+            return loader
+        try:
+            if not loader.is_package(package.__name__):
+                return None
+        except AttributeError:
+            return None
+
+        cog_list = []
+        spec_list = []
+        for importer, modname, ispkg in pkgutil.walk_packages(package.__path__):
+            import_path = f"{package.__name__}.{modname}"
+            if ispkg:
+                spec = pkgutil._get_spec(importer, modname)
+                importlib._bootstrap._load(spec)
+                spec_list.append(spec)
+            else:
+                cog_list.append(import_path)
+
+        # remove sys.modules clutter created during cog search
+        for spec in spec_list:
+            del sys.modules[spec.name]
+        return cog_list
 
     def get_cog(self, name):
         """Gets the cog instance requested.
