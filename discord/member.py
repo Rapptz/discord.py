e75b60de2a7427e6783414862527a507bd0d63d1
Carl Groth <callegroth@gmail.com>
2018-05-27 16:41:31 +0200

    Fix member.top_role not accounting for role hierarchy changes

--- discord/member.py
+++ discord/member.py
@@ -308,7 +308,7 @@ class Member(discord.abc.Messageable, _BaseUser):
         This is useful for figuring where a member stands in the role
         hierarchy chain.
         """
-        return self.roles[-1]
+        return sorted(self.roles, key=lambda r: r.position, reverse=True)[0]
 
     @property
     def guild_permissions(self):
