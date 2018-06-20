6c836fab63e2006a3c00d8b0bca0e76686e2e6b0
Ben Mintz <bmintz@protonmail.com>
2018-06-20 13:06:32 -0500

    Message.jump_to_url: fix the format
    The current URL format in Discord Stable for Desktop and Android has replaced ?jump= with /.
    Updates Message.jump_to_url to reflect this.

--- discord/message.py
+++ discord/message.py
@@ -437,7 +437,7 @@ class Message:
     def jump_to_url(self):
         """:class:`str`: Returns a URL that allows the client to jump to this message."""
         guild_id = getattr(self.guild, 'id', '@me')
-        return 'https://discordapp.com/channels/{0}/{1.channel.id}?jump={1.id}'.format(guild_id, self)
+        return 'https://discordapp.com/channels/{0}/{1.channel.id}/{1.id}'.format(guild_id, self)
 
     @utils.cached_slot_property('_cs_system_content')
     def system_content(self):
