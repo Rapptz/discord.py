d68af294a2281d02448b9626ee177777715c9162
Rapptz <rapptz@gmail.com>
2019-04-06 23:02:49 -0400

    Fix regex substitution in utils.escape_mentions

--- discord/utils.py
+++ discord/utils.py
@@ -405,4 +405,4 @@ def escape_mentions(text):
     :class:`str`
         The text with the mentions removed.
     """
-    return re.sub(r'@(everyone|here|[!&]?[0-9]{17,21})', r'@\u200b\1', text)
+    return re.sub(r'@(everyone|here|[!&]?[0-9]{17,21})', '@\u200b\\1', text)
