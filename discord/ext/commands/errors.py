ac5c08c42f02d3246c298adc9b0a37b391bd606e
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-04-09 09:30:21 -0700

    Added changes to the apppropriate __all__ lists.

--- discord/ext/commands/errors.py
+++ discord/ext/commands/errors.py
@@ -28,15 +28,15 @@ from discord.errors import DiscordException
 
 
 __all__ = ['CommandError', 'MissingRequiredArgument', 'BadArgument',
-           'NoPrivateMessage', 'CheckFailure', 'CommandNotFound',
-           'DisabledCommand', 'CommandInvokeError', 'TooManyArguments',
-           'UserInputError', 'CommandOnCooldown', 'NotOwner',
-           'MissingPermissions', 'BotMissingPermissions', 'ConversionError',
-           'BadUnionArgument', 'ArgumentParsingError',
+           'PrivateMessageOnly', 'NoPrivateMessage', 'CheckFailure',
+           'CommandNotFound' ,'DisabledCommand', 'CommandInvokeError',
+           'TooManyArguments', 'UserInputError', 'CommandOnCooldown',
+           'NotOwner', 'MissingPermissions', 'BotMissingPermissions',
+           'ConversionError', 'BadUnionArgument', 'ArgumentParsingError',
            'UnexpectedQuoteError', 'InvalidEndOfQuotedStringError',
            'ExpectedClosingQuoteError', 'ExtensionError', 'ExtensionAlreadyLoaded',
            'ExtensionNotLoaded', 'NoEntryPointError', 'ExtensionFailed',
-           'ExtensionNotFound' ]
+           'ExtensionNotFound']
 
 class CommandError(DiscordException):
     r"""The base exception type for all command related errors.
