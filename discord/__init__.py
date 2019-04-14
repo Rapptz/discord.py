14a593b026e71ac67fef02c4aaa6dbf520e0498d
Rapptz <rapptz@gmail.com>
2019-04-14 17:58:56 -0400

    Bump version to 1.1.0a

--- discord/__init__.py
+++ discord/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'discord'
 __author__ = 'Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2019 Rapptz'
-__version__ = '1.0.1'
+__version__ = '1.1.0a'
 
 from collections import namedtuple
 import logging
@@ -53,7 +53,7 @@ from .raw_models import *
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=1, minor=0, micro=1, releaselevel='final', serial=0)
+version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel='alpha', serial=0)
 
 try:
     from logging import NullHandler
