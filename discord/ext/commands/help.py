4ca934eb22432f601a2349a94b41b341938f6966
Rapptz <rapptz@gmail.com>
2019-04-19 18:05:29 -0400

    [commands] Missing an extra ] in the documented return type.

--- discord/ext/commands/help.py
+++ discord/ext/commands/help.py
@@ -598,7 +598,7 @@ class HelpCommand:
 
         Parameters
         ------------
-        mapping: Mapping[Optional[:class:`Cog`], List[:class:`Command`]
+        mapping: Mapping[Optional[:class:`Cog`], List[:class:`Command`]]
             A mapping of cogs to commands that have been requested by the user for help.
             The key of the mapping is the :class:`~.commands.Cog` that the command belongs to, or
             ``None`` if there isn't one, and the value is a list of commands that belongs to that cog.
