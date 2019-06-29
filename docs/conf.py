d747f2c88baeb9ba3f1eb9e1421c7a34b3ec99e5
Rapptz <rapptz@gmail.com>
2019-06-29 02:36:26 -0400

    Use a new scorer to improve search results.
    This makes the other pages take less priority than the API results.
    It's kind of biased, but hopefully it helps out.

--- docs/conf.py
+++ docs/conf.py
@@ -234,7 +234,7 @@ html_static_path = ['_static']
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
-#html_search_scorer = 'scorer.js'
+html_search_scorer = '_static/scorer.js'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'discord.pydoc'
