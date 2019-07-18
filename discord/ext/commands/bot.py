Rapptz/discord.py

commit 6f71552c508c61d9b6bf024fc259063ad056b7c4

[commands] Don't update sys.modules with a stale reference.

discord/ext/commands/bot.py
@@ -605,7 +605,7 @@ class BotBase(GroupMixin):
             self._call_module_finalizers(lib, key)
             raise errors.ExtensionFailed(key, e) from e
         else:
-            sys.modules[key] = self.__extensions[key] = lib
+            self.__extensions[key] = lib
 
     def load_extension(self, name):
         """Loads an extension.

commit 48f8a5536d16744b7ca0e51b3c64dc94ab04d553

[commands] update sys.modules in load_extension again
6f71552c508c61d9b6bf024fc259063ad056b7c4 introduced a regression: loading a module that is not in a package
does not add it to sys.modules. Updating sys.modules is required after all.

discord/ext/commands/bot.py
@@ -588,19 +588,23 @@ class BotBase(GroupMixin):
     def _load_from_module_spec(self, spec, key):
         # precondition: key not in self.__extensions
         lib = importlib.util.module_from_spec(spec)
+        sys.modules[key] = lib
         try:
             spec.loader.exec_module(lib)
         except Exception as e:
+            del sys.modules[key]
             raise errors.ExtensionFailed(key, e) from e
 
         try:
             setup = getattr(lib, 'setup')
         except AttributeError:
+            del sys.modules[key]
             raise errors.NoEntryPointError(key)
 
         try:
             setup(self)
         except Exception as e:
+            del sys.modules[key]
             self._remove_module_references(lib.__name__)
             self._call_module_finalizers(lib, key)
             raise errors.ExtensionFailed(key, e) from e