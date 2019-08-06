125a5676b804970332aab8fbd3d7f72f07a07b09
Capn <38225872+CapnS@users.noreply.github.com>
2019-08-06 15:33:42 -0500

    Add permissions_synced to abc.GuildChannel
    This allows users to check whether or not the permissions for a Guild Channel are synced with the permissions for its category. Discord automatically syncs the permissions when the overwrites are equal so just checking if the two overwrites are equal will determine if they are synced.

--- discord/abc.py
+++ discord/abc.py
@@ -386,6 +386,16 @@ class GuildChannel:
         """
         return self.guild.get_channel(self.category_id)
 
+    @property
+    def permissions_synced(self):
+        """:class:`bool`: Whether or not the permissions for this channel are synced with the
+        category it belongs to.
+
+        If there is no category then this is ``False``.
+        """
+        category = self.guild.get_channel(self.category_id)
+        return category and category._overwrites == self._overwrites
+
     def permissions_for(self, member):
         """Handles permission resolution for the current :class:`~discord.Member`.
 
