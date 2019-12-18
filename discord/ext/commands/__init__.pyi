c542e077829be77de78a1590d639e3e16c8a4e04
Bryan Forbes <bryan@reigndropsfall.net>
2019-12-18 10:39:41 -0600

    Export _CheckDecorator from commands so user-defined checks can use it

--- discord/ext/commands/__init__.pyi
+++ discord/ext/commands/__init__.pyi
@@ -1,6 +1,7 @@
 from .bot import Bot as Bot, AutoShardedBot as AutoShardedBot, when_mentioned as when_mentioned, when_mentioned_or as when_mentioned_or
 from .context import Context as Context
 from .core import *
+from .core import _CheckDecorator as _CheckDecorator
 from .errors import *
 from .help import *
 from .converter import *
