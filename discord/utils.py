5de7d1b9cd55a129c66c5bf9d835dab84419ee4a
Josh B <josh.ja.butt@gmail.com>
2020-01-21 23:44:28 +1000

    Fix issue where sleeping for an extended period on python 3.5 would cause an exception

--- discord/utils.py
+++ discord/utils.py
@@ -43,6 +43,7 @@ from .errors import InvalidArgument
 from .object import Object
 
 DISCORD_EPOCH = 1420070400000
+MAX_ASYNCIO_SECONDS = 3456000
 
 class cached_property:
     def __init__(self, function):
@@ -354,6 +355,9 @@ async def sleep_until(when):
         when = when.replace(tzinfo=datetime.timezone.utc)
     now = datetime.datetime.now(datetime.timezone.utc)
     delta = (when - now).total_seconds()
+    while delta > MAX_ASYNCIO_SECONDS:
+        await asyncio.sleep(MAX_ASYNCIO_SECONDS)
+        delta -= MAX_ASYNCIO_SECONDS
     await asyncio.sleep(max(delta, 0))
 
 def valid_icon_size(size):
