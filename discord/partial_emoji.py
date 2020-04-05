1507512852d18f21ae131c2cc02d0027b3d711c7
NCPlayz <chowdhurynadir0@outlook.com>
2020-04-05 21:40:00 +0100

    animated key is never given to reaction_remove(_emoji)

--- discord/partial_emoji.py
+++ discord/partial_emoji.py
@@ -88,8 +88,8 @@ class PartialEmoji(_EmojiTag):
         return o
 
     @classmethod
-    def with_state(cls, state, *, animated, name, id=None):
-        self = cls(animated=animated, name=name, id=id)
+    def with_state(cls, state, *, name, animated=False, id=None):
+        self = cls(name=name, animated=animated, id=id)
         self._state = state
         return self
 
