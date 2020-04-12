acab11121315892daf4e0bd68fbce9ca7512eeec
JohnyTheCarrot <tuurmartens4@gmail.com>
2020-04-12 21:09:43 +0200

    revert reply.py changes

--- examples/reply.py
+++ examples/reply.py
@@ -11,9 +11,9 @@ class MyClient(discord.Client):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
-        print(message.author.flags)
+
         if message.content.startswith('!hello'):
             await message.channel.send('Hello {0.author.mention}'.format(message))
 
 client = MyClient()
-client.run('NDAwNjI4NDI1ODM4MDM0OTU0.XpNiBw.WKS40XAoJW5qZdpiLGX22b1yDAI')
+client.run('token')
