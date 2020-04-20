69cf8c3493e34bdeac848423d377b6023b8ad3a8
Bryan Forbes <bryan@reigndropsfall.net>
2020-04-20 13:37:02 -0500

    Update typings

--- discord/object.pyi
+++ discord/object.pyi
@@ -1,9 +1,11 @@
-from .mixins import Hashable
 import datetime
+from typing import Text, SupportsInt, Union
+from builtins import _SupportsIndex
+from .mixins import Hashable
 
 class Object(Hashable):
     id: int
 
-    def __init__(self, id: int) -> None: ...
+    def __init__(self, id: Union[Text, bytes, SupportsInt, _SupportsIndex]) -> None: ...
     @property
     def created_at(self) -> datetime.datetime: ...
