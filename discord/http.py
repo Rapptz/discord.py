7487a628a798cc0e5fed930cd41667ed8aecf16d
NCPlayz <chowdhurynadir0@outlook.com>
2020-05-22 19:39:42 +0100

    Implement some missing attributes of `Guild` (and `Guild.edit`)

--- discord/http.py
+++ discord/http.py
@@ -620,7 +620,8 @@ class HTTPClient:
                       'afk_channel_id', 'splash', 'verification_level',
                       'system_channel_id', 'default_message_notifications',
                       'description', 'explicit_content_filter', 'banner',
-                      'system_channel_flags')
+                      'system_channel_flags', 'rules_channel_id',
+                      'public_updates_channel_id')
 
         payload = {
             k: v for k, v in fields.items() if k in valid_keys
