5fbb340339ec5ff99575a76f975a7f549842b7c8
Rapptz <rapptz@gmail.com>
2020-05-25 11:55:13 -0400

    Bump Sphinx to 2.4.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open('README.rst') as f:
 extras_require = {
     'voice': ['PyNaCl>=1.3.0,<1.5'],
     'docs': [
-        'sphinx==1.8.5',
+        'sphinx==2.4.4',
         'sphinxcontrib_trio==1.1.1',
         'sphinxcontrib-websupport',
     ]
