37d990148d989a3361d95086bb6b0beb1fe5bfe9
Josh B <josh.ja.butt@gmail.com>
2020-05-27 17:21:44 +1000

    Add sans serif font toggle

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -3,7 +3,7 @@
 let activeModal = null;
 let activeLink = null;
 let bottomHeightThreshold, sections;
-let settings;
+let settingsModal;
 
 function closeModal(modal) {
   activeModal = null;
@@ -19,10 +19,24 @@ function openModal(modal) {
   modal.style.removeProperty('display');
 }
 
+function updateSetting(element) {
+  localStorage.setItem(element.name, element.checked);
+}
+
+function setFont(useSans) {
+  if (useSans === true) {
+    document.body.classList.add('sans');
+  } else {
+    document.body.classList.remove('sans');
+  }
+}
+
+const settings = [['useSansFont', setFont]];
+
 document.addEventListener('DOMContentLoaded', () => {
   bottomHeightThreshold = document.documentElement.scrollHeight - 30;
   sections = document.querySelectorAll('div.section');
-  settings = document.querySelector('div#settings.modal')
+  settingsModal = document.querySelector('div#settings.modal')
 
   const tables = document.querySelectorAll('.py-attribute-table[data-move-to-id]');
   tables.forEach(table => {
@@ -31,6 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // insert ourselves after the element
     parent.insertBefore(table, element.nextSibling);
   });
+
+  settings.forEach(([settingName, settingFunction]) => {
+    let settingValue = JSON.parse(localStorage.getItem(settingName));
+    settingFunction(settingValue);
+    let element = document.querySelector(`input[name=${settingName}]`);
+    if (element) {
+      element.checked = settingValue === true;
+    }
+  });
+
 });
 
 window.addEventListener('scroll', () => {
