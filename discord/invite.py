d426a620ce4bc40eba0ec227ded51128ad59abcd
Anurag <30367300+anurag-7@users.noreply.github.com>
2020-06-06 15:30:13 +0530

    Change PartialGuild.icon_url_as to mimic Guild.icon_url_as

--- discord/invite.py
+++ discord/invite.py
@@ -154,11 +154,15 @@ class PartialInviteGuild:
     def icon_url(self):
         """:class:`Asset`: Returns the guild's icon asset."""
         return self.icon_url_as()
+    
+    def is_icon_animated(self):
+        """:class:`Asset`: Returns the guild's icon asset."""
+        return bool(self.icon and self.icon.startswith('a_'))
 
-    def icon_url_as(self, *, format='webp', size=1024):
+    def icon_url_as(self, *, format=None, static_format='webp', size=1024):
         """The same operation as :meth:`Guild.icon_url_as`."""
-        return Asset._from_guild_image(self._state, self.id, self.icon, 'icons', format=format, size=size)
-
+        return Asset._from_guild_icon(self._state, self, format=format, static_format=static_format, size=size)
+    
     @property
     def banner_url(self):
         """:class:`Asset`: Returns the guild's banner asset."""
