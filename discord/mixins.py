858120a4edf0f9c8c8f902f171ac5cd3ef62a661
Znunu <61394004+Znunu@users.noreply.github.com>
2020-06-07 20:16:25 +0200

    Mixins is refractored
    From what I could see __nq__ was just the inverse of __eq__ so I removed it. EqualityComparable wasn't used anywhere except for Hashable, and I can't imagine anything else, since they both rely on the same thing, object having an id. So I merged them. Frankly, I think it's kind of silly to have so few lines of code in a file itself, but what do I know.

--- discord/mixins.py
+++ discord/mixins.py
@@ -24,19 +24,12 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-class EqualityComparable:
-    __slots__ = ()
 
+class Hashable:
+    __slots__ = ()
+    
     def __eq__(self, other):
         return isinstance(other, self.__class__) and other.id == self.id
-
-    def __ne__(self, other):
-        if isinstance(other, self.__class__):
-            return other.id != self.id
-        return True
-
-class Hashable(EqualityComparable):
-    __slots__ = ()
-
+    
     def __hash__(self):
         return self.id >> 22
