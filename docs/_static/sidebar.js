4e0a677599d16945315c786f41d2a26f6a56fd24
NCPlayz <chowdhurynadir0@outlook.com>
2020-06-27 15:33:04 +0100

    complete collapsible sidebar

--- docs/_static/sidebar.js
+++ docs/_static/sidebar.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     let sidebar = document.getElementById('sidebar');
     let toc = sidebar.querySelector('ul');
-    let allReferences = toc.querySelectorAll('a.reference.internal');
+    let allReferences = toc.querySelectorAll('a.reference.internal:not([href="#"])');
 
     for (let ref of allReferences) {
         let next = ref.nextElementSibling;
         if (next && next.tagName === "UL") {
             let icon = document.createElement('span');
-            icon.className = 'fas fa-chevron-down';
+            icon.className = 'fas fa-chevron-down collapsible-arrow';
             icon.addEventListener('click', () => {
                 if (icon.classList.contains('fa-chevron-down')) {
                     icon.classList.remove('fa-chevron-down'); // safari doesn't support .replace yet(!)
-                    icon.classList.add('fa-chevron-right');
-                    icon.nextElementSibling.style.display = "block";
+                    icon.classList.add('fa-chevron-left');
+                    let children = icon.nextElementSibling; 
+                    // <arrow><heading> 
+                    // --> <arrow><children>
+                    children.style.display = "none";
                 } else {
-                    icon.classList.remove('fa-chevron-right'); // safari doesn't support .replace yet(!)
+                    icon.classList.remove('fa-chevron-left');
                     icon.classList.add('fa-chevron-down');
-                    icon.nextElementSibling.style.display = "none";
+                    let children = icon.nextElementSibling; 
+                    children.style.display = "block";
                 }
             })
-            ref.parentNode.insertBefore(icon, ref);
+            ref.parentNode.insertBefore(icon, ref.nextElementSibling);
         }
     }
 });
