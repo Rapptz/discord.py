de7c96333ced03fa7b392b2437d0702dc0508890
Rapptz <rapptz@gmail.com>
2020-06-27 03:45:27 -0400

    Update Message.system_content join strings to be up to date.
    Fixes #4211

--- discord/message.py
+++ discord/message.py
@@ -187,7 +187,7 @@ class Attachment:
             .. versionadded:: 1.4
         spoiler: :class:`bool`
             Whether the file is a spoiler.
-            
+
             .. versionadded:: 1.4
 
         Raises
@@ -663,45 +663,19 @@ class Message:
 
         if self.type is MessageType.new_member:
             formats = [
-                "{0} just joined the server - glhf!",
-                "{0} just joined. Everyone, look busy!",
-                "{0} just joined. Can I get a heal?",
-                "{0} joined your party.",
-                "{0} joined. You must construct additional pylons.",
-                "Ermagherd. {0} is here.",
-                "Welcome, {0}. Stay awhile and listen.",
-                "Welcome, {0}. We were expecting you ( ͡° ͜ʖ ͡°)",
+                "{0} joined the party.",
+                "{0} is here.",
                 "Welcome, {0}. We hope you brought pizza.",
-                "Welcome {0}. Leave your weapons by the door.",
                 "A wild {0} appeared.",
-                "Swoooosh. {0} just landed.",
-                "Brace yourselves. {0} just joined the server.",
-                "{0} just joined... or did they?",
-                "{0} just arrived. Seems OP - please nerf.",
+                "{0} just landed.",
                 "{0} just slid into the server.",
-                "A {0} has spawned in the server.",
-                "Big {0} showed up!",
-                "Where’s {0}? In the server!",
-                "{0} hopped into the server. Kangaroo!!",
-                "{0} just showed up. Hold my beer.",
-                "Challenger approaching - {0} has appeared!",
-                "It's a bird! It's a plane! Nevermind, it's just {0}.",
-                "It's {0}! Praise the sun! \\[T]/",
-                "Never gonna give {0} up. Never gonna let {0} down.",
-                "{0} has joined the battle bus.",
-                "Cheers, love! {0}'s here!",
-                "Hey! Listen! {0} has joined!",
-                "We've been expecting you {0}",
-                "It's dangerous to go alone, take {0}!",
-                "{0} has joined the server! It's super effective!",
-                "Cheers, love! {0} is here!",
-                "{0} is here, as the prophecy foretold.",
-                "{0} has arrived. Party's over.",
-                "Ready player {0}",
-                "{0} is here to kick butt and chew bubblegum. And {0} is all out of gum.",
-                "Hello. Is it {0} you're looking for?",
-                "{0} has joined. Stay a while and listen!",
-                "Roses are red, violets are blue, {0} joined this server with you",
+                "{0} just showed up!",
+                "Welcome {0}. Say hi!",
+                "{0} hopped into the server.",
+                "Everyone welcome {0}!",
+                "Glad you're here, {0}.",
+                "Good to see you, {0}.",
+                "Yay you made it, {0}!",
             ]
 
             # manually reconstruct the epoch with millisecond precision, because
