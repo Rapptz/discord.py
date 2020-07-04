84b8fbb2587cd38f95436894dd7a746cfac62c80
NCPlayz <chowdhurynadir0@outlook.com>
2020-07-04 17:58:38 +0100

    (typo)

--- discord/template.py
+++ discord/template.py
@@ -58,7 +58,7 @@ class Template:
     code: :class:`str`
         The template code.
     uses: :class:`int`
-        How many time the template has been used.
+        How many times the template has been used.
     name: :class:`str`
         The name of the template.
     description: :class:`str`
