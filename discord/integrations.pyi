d690f98bf94ebb66ef537cdbf6c9954014eb6c77
Bryan Forbes <bryan@reigndropsfall.net>
2020-07-06 10:29:12 -0500

    Update typings

--- discord/integrations.pyi
+++ discord/integrations.pyi
@@ -17,14 +17,14 @@ class Integration:
     type: str
     enabled: bool
     syncing: bool
-    role: Optional[Role]
-    enable_emoticons: bool
+    role: Role
+    enable_emoticons: Optional[bool]
     expire_behaviour: ExpireBehaviour
     expire_behavior: ExpireBehaviour
     expire_grace_period: int
     user: User
     account: IntegrationAccount
-    synced_at: Optional[datetime.datetime]
+    synced_at: datetime.datetime
     async def edit(self, *, expire_behaviour: ExpireBehaviour = ..., expire_behavior: ExpireBehaviour = ...,
                    expire_grace_period: int = ..., enable_emoticons: bool = ...) -> None: ...
     async def sync(self) -> None: ...
