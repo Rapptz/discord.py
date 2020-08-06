360fc123c6fdedb1bc796e1097cd571cccc88052
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2020-08-06 03:04:09 +0100

    Add remaining user flags

--- discord/enums.py
+++ discord/enums.py
@@ -396,12 +396,15 @@ class UserFlags(Enum):
     partner = 2
     hypesquad = 4
     bug_hunter = 8
+    mfa_sms = 16
+    premium_promo_dismissed = 32
     hypesquad_bravery = 64
     hypesquad_brilliance = 128
     hypesquad_balance = 256
     early_supporter = 512
     team_user = 1024
     system = 4096
+    has_unread_urgent_messages = 8192
     bug_hunter_level_2 = 16384
     verified_bot = 65536
     verified_bot_developer = 131072
