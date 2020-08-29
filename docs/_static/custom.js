8717096ede57e04ca710ed1f8aabc03df0ce8e2c
Josh <josh.ja.butt@gmail.com>
2020-08-29 15:27:36 +1000

    Set back to light when user does not prefer dark

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -64,6 +64,8 @@ function setTheme(value) {
   if (value === "automatic") {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.documentElement.setAttribute(`data-theme`, "dark");
+    } else{
+      document.documentElement.setAttribute(`data-theme`, "light");
     }
   }
   else {
