af88fc19b3c062e27ffe354df77413158f8a2813
Skye Im <fluffyterran@gmail.com>
2020-09-08 04:29:02 +0200

    [commands] Update Bot.command() decorator docs

--- discord/ext/commands/core.py
+++ discord/ext/commands/core.py
@@ -1237,6 +1237,11 @@ class GroupMixin:
     def command(self, *args, **kwargs):
         """A shortcut decorator that invokes :func:`.command` and adds it to
         the internal command list via :meth:`~.GroupMixin.add_command`.
+
+        Returns
+        --------
+        Callable[..., :class:`Command`]
+            A decorator that converts the provided method into a Command, adds it to the bot, then returns it
         """
         def decorator(func):
             kwargs.setdefault('parent', self)
