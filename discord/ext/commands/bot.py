c29c47a5ea54ca38e69a336f0657e44bc8573f3c
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2020-09-23 00:12:46 -0700

    Revert "[commands] type Bot.command_prefix"
    This reverts commit a33c47983778bb215bce53fc03aa1476f599b893.

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -954,7 +954,7 @@ class Bot(BotBase, discord.Client):
 
     Attributes
     -----------
-    command_prefix: Union[:class:`str`, Iterable[:class:`str`], Callable[[:class:`Bot`, :class:`.Message`], Union[:class:`str`, Iterable[:class:`str`]]]]
+    command_prefix
         The command prefix is what the message content must contain initially
         to have a command invoked. This prefix could either be a string to
         indicate what the prefix should be, or a callable that takes in the bot
