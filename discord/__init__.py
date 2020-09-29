6d1bcf89da7b35b39e319544c2f4fd2a8e2e60c1
Rapptz <rapptz@gmail.com>
2020-09-28 20:57:10 -0400

    Version bump to v1.5.0

--- discord/__init__.py
+++ discord/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'discord'
 __author__ = 'Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2020 Rapptz'
-__version__ = '1.5.0a'
+__version__ = '1.5.0'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
@@ -61,7 +61,7 @@ from .team import *
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel='alpha', serial=0)
+version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel='final', serial=0)
 
 try:
     from logging import NullHandler
