60758edf1eb62b3890b6ab212b4c47ccf350c6da
Josh <josh.ja.butt@gmail.com>
2020-10-13 00:22:21 +1000

    Update colour.py

--- discord/colour.py
+++ discord/colour.py
@@ -238,7 +238,7 @@ class Colour:
     @classmethod
     def dark_theme(cls):
         """A factory method that returns a :class:`Colour` with a value of ``0x36393F``.
-        Will appear transparent on Discord's dark theme and be the text colour on Discord's light theme.
+        This will appear transparent on Discord's dark theme.
         
         .. versionadded:: 1.5
         """
