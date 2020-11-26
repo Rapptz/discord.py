a84565d1a4b10aa124fd88b47dc475057604f5ea
kippiii <ianorzel@gmail.com>
2020-11-26 15:41:16 -0500

    Condensing if statements in reaction events

--- examples/reaction_roles.py
+++ examples/reaction_roles.py
@@ -17,22 +17,18 @@ emoji_to_role = {
 @bot.event
 async def on_raw_reaction_add(payload):
     """Gives a role based on a reaction emoji."""
-    if payload.guild_id:
-        if payload.emoji.name in emoji_to_role:
-            if payload.message_id == role_message_id:
-                guild = bot.get_guild(payload.guild_id)
-                role = guild.get_role(emoji_to_role[payload.emoji.name])
-                await payload.member.add_roles(role)
+    if payload.message_id == role_message_id and payload.emoji.name in emoji_to_role:
+        guild = bot.get_guild(payload.guild_id)
+        role = guild.get_role(emoji_to_role[payload.emoji.name])
+        await payload.member.add_roles(role)
 
 @bot.event
 async def on_raw_reaction_remove(payload):
     """Removes a role based on a reaction emoji."""
-    if payload.guild_id:
-        if payload.emoji.name in emoji_to_role:
-            if payload.message_id == role_message_id:
-                guild = bot.get_guild(payload.guild_id)
-                role = guild.get_role(emoji_to_role[payload.emoji.name])
-                member = guild.get_member(payload.user_id)
-                await member.remove_roles(role)
+    if payload.message_id == role_message_id and payload.emoji.name in emoji_to_role:
+        guild = bot.get_guild(payload.guild_id)
+        role = guild.get_role(emoji_to_role[payload.emoji.name])
+        member = guild.get_member(payload.user_id)
+        await member.remove_roles(role)
 
 bot.run("token")
