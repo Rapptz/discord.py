7100852d6261911f5cf6a03bcdfffdc01287dc88
Kippiii <ianorzel@gmail.com>
2020-11-27 17:40:42 -0500

    Updating style in reaction roles example
    Co-authored-by: Danny <Rapptz@users.noreply.github.com>

--- examples/reaction_roles.py
+++ examples/reaction_roles.py
@@ -17,10 +17,32 @@ emoji_to_role = {
 @bot.event
 async def on_raw_reaction_add(payload):
     """Gives a role based on a reaction emoji."""
-    if payload.message_id == role_message_id and payload.emoji.name in emoji_to_role:
-        guild = bot.get_guild(payload.guild_id)
-        role = guild.get_role(emoji_to_role[payload.emoji.name])
+    # Make sure that the message the user is reacting to is the one we care about
+    if payload.message_id != role_message_id:
+        return
+    
+    try:
+        role_id = emoji_to_role[str(payload.emoji)]
+    except KeyError:
+        # If the emoji isn't the one we care about then exit as well.
+        return
+    
+    guild = bot.get_guild(payload.guild_id)
+    if guild is None:
+        # Check if we're still in the guild and it's cached.
+        return
+
+    role = guild.get_role(role_id)
+    if role is None:
+        # Make sure the role still exists and is valid.
+        return
+
+    try:
+        # Finally add the role
         await payload.member.add_roles(role)
+    except discord.HTTPException:
+        # If we want to do something in case of errors we'd do it here.
+        pass
 
 @bot.event
 async def on_raw_reaction_remove(payload):
