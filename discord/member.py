85b23231582eb0fb8c3318db237dbb58b912f122
Perry Cate <ncate@alumni.princeton.edu>
2021-01-09 22:56:51 -0500

    Update add_roles docstring to mention role order.

--- discord/member.py
+++ discord/member.py
@@ -643,7 +643,8 @@ class Member(discord.abc.Messageable, _BaseUser):
         Gives the member a number of :class:`Role`\s.
 
         You must have the :attr:`~Permissions.manage_roles` permission to
-        use this.
+        use this, and the added :class:`Role`\s must appear lower in the list
+        of roles than the highest role of the caller.
 
         Parameters
         -----------
