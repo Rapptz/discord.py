69bdc3a1849407f64ef93bd2e5119e54d6cd2208
Nihaal Sangha <nihaal.git@gmail.com>
2021-01-15 10:28:11 +0000

    Change copyright year to present

--- discord/flags.py
+++ discord/flags.py
@@ -3,7 +3,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2015-2020 Rapptz
+Copyright (c) 2015-present Rapptz
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
@@ -938,4 +938,3 @@ class MemberCacheFlags(BaseFlags):
     @property
     def _online_only(self):
         return self.value == 1
-
