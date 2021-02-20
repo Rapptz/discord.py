ff26d045024e9497befa3a404aa6fcc43c911e4a
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2021-02-20 16:47:57 +0000

    Add a converters example for typehinted and manual conversions.

---
+++ examples/converters.py
@@ -0,0 +1,78 @@
+import discord
+from discord.ext import commands
+
+# We will need the members intents at the minimum to use the Member converter.
+intents = discord.Intents.all()
+
+# Construct the Bot instance.
+bot = commands.Bot("!", intents=intents)
+
+
+@bot.command()
+async def userinfo(ctx: commands.Context, user: discord.User):
+    # In the command signature about, you can see that the `user`
+    # parameter is typehinted to `discord.User`. This means that
+    # during command invocation we will attempt to convert
+    # the value passed as `user` to a `discord.User` instance.
+
+    # If the conversion is successful we will have a User instance
+    # and can do the following:
+    return await ctx.send(user.name)
+
+@userinfo.error
+async def userinfo_error(ctx: commands.Context, error: Exception):
+    # if the conversion above fails for any reason, it will raise `commands.errors.BadArgument`
+    # so we handle this in this error handler
+    if isinstance(error, commands.BadArgument):
+        return await ctx.send("Failed to convert the argument for `user` to `discord.User`.")
+
+# Manual use of converters will follow
+
+@bot.command()
+async def channel_or_member(ctx: commands.Context, argument: int):
+    # Preface - this could be done with `Guild.get_X` calls, but this is for the purpose of an example.
+
+    # Above you can see we want `argument` to be of `int` type
+    # This will attempt to cast whatever `argument` is to `int`
+    # with a base of 10: `int(argument, 10)`
+    # NOTE: There are more builtin type converters, like `float` and `bool`.
+
+    # If you are doing this kind of thing commonly, consider making a Custom Converter
+    # https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#advanced-converters
+
+    # If for any reason you have an ID, but it may be of one of more
+    # items, like a TextChannel or Member, you can manually call the converters
+    # like the following:
+    member_converter = commands.MemberConverter()
+    try:
+        # Try and convert to a Member instance.
+        member = await member_converter.convert(ctx, argument)
+    except commands.MemberNotFound:
+        # Could not convert to a member instance
+        pass
+    else:
+        # We have our `member` so lets return here.
+        return await ctx.send("Member found: {}".format(member))
+
+    # Do the same for TextChannel...
+    textchannel_converter = commands.TextChannelConverter()
+    try:
+        channel = await textchannel_converter.convert(ctx, argument)
+    except commands.ChannelNotFound:
+        pass
+    else:
+        return await ctx.send("Channel found: {}".format(channel))
+
+    return await ctx.send("No member or channel matching this ID was found.")
+
+# Builtin type converters
+@bot.command()
+async def trial_converter(ctx: commands.Context, number: int, maybe: bool):
+    # We want and int and a bool parameter here.
+    # Bool is a slightly special case, as shown here:
+    # https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#bool
+
+    return await ctx.send("Number: {} -- Bool: {}.".format(number, maybe))
+
+token = "your token here"
+bot.run(token)
