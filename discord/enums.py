6f748e5da59e5c98504c14d52fcc00a8cce7041a
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2021-02-23 08:57:11 +0000

    Add remaining v6 message types

--- discord/enums.py
+++ discord/enums.py
@@ -163,19 +163,24 @@ class ChannelType(Enum):
         return self.name
 
 class MessageType(Enum):
-    default                    = 0
-    recipient_add              = 1
-    recipient_remove           = 2
-    call                       = 3
-    channel_name_change        = 4
-    channel_icon_change        = 5
-    pins_add                   = 6
-    new_member                 = 7
-    premium_guild_subscription = 8
-    premium_guild_tier_1       = 9
-    premium_guild_tier_2       = 10
-    premium_guild_tier_3       = 11
-    channel_follow_add         = 12
+    default                                      = 0
+    recipient_add                                = 1
+    recipient_remove                             = 2
+    call                                         = 3
+    channel_name_change                          = 4
+    channel_icon_change                          = 5
+    pins_add                                     = 6
+    new_member                                   = 7
+    premium_guild_subscription                   = 8
+    premium_guild_tier_1                         = 9
+    premium_guild_tier_2                         = 10
+    premium_guild_tier_3                         = 11
+    channel_follow_add                           = 12
+    guild_stream                                 = 13
+    guild_discovery_disqualified                 = 14
+    guild_discovery_requalified                  = 15
+    guild_discovery_grace_period_initial_warning = 16
+    guild_discovery_grace_period_final_warning   = 17
 
 class VoiceRegion(Enum):
     us_west       = 'us-west'
