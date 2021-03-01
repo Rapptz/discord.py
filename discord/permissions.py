259c3909a8ef9041cc8f80ac24ca73dadd3db415
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2021-03-01 14:57:28 +0100

    Fix Permissions.all_channel documentation to link attributes

--- discord/permissions.py
+++ discord/permissions.py
@@ -149,15 +149,16 @@ class Permissions(BaseFlags):
         """A :class:`Permissions` with all channel-specific permissions set to
         ``True`` and the guild-specific ones set to ``False``. The guild-specific
         permissions are currently:
-        - manage_emojis
-        - view_audit_log
-        - view_guild_insights
-        - manage_guild
-        - change_nickname
-        - manage_nicknames
-        - kick_members
-        - ban_members
-        - administrator
+        
+        - :attr:`manage_emojis`
+        - :attr:`view_audit_log`
+        - :attr:`view_guild_insights`
+        - :attr:`manage_guild`
+        - :attr:`change_nickname`
+        - :attr:`manage_nicknames`
+        - :attr:`kick_members`
+        - :attr:`ban_members`
+        - :attr:`administrator`
         
         .. versionchanged:: 1.7
            Added :attr:`stream`, :attr:`priority_speaker` and :attr:`use_slash_commands` permissions.
@@ -208,7 +209,7 @@ class Permissions(BaseFlags):
         """A factory method that creates a :class:`Permissions` with all
         "Advanced" permissions from the official Discord UI set to ``True``.
         
-        .. versionadded: 1.7
+        .. versionadded:: 1.7
         """
         return cls(1 << 3)
 
