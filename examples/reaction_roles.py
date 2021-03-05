9974a448873d35bc52c2e5b60eab4a10adf3b9d6
NCPlayz <chowdhurynadir0@outlook.com>
2021-03-05 14:08:00 +0000

    improve the example properly

--- examples/reaction_roles.py
+++ examples/reaction_roles.py
@@ -8,25 +8,28 @@ class RoleReactClient(discord.Client):
 
         self.role_message_id = 0  # ID of message that can be reacted to to add role
         self.emoji_to_role = {
-            'partial_emoji_1': 0,  # ID of role associated with partial emoji object 'partial_emoji_1'
-            'partial_emoji_2': 0  # ID of role associated with partial emoji object 'partial_emoji_2'
+            '🔴': 0,  # ID of the role associated with unicode emoji '🔴'
+            '🟡': 0,  # ID of the role associated with unicode emoji '🟡'
+            0: 0, # ID of the role associated with a partial emoji's id.
         }
 
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         """Gives a role based on a reaction emoji."""
         # Make sure that the message the user is reacting to is the one we care about
         if payload.message_id != self.role_message_id:
             return
 
-        try:
-            role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-            # If the emoji isn't the one we care about then exit as well.
-            return
-
         guild = self.get_guild(payload.guild_id)
         if guild is None:
             # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            # If it is a unicode emoji, it should use .name, otherwise it should use .id.
+            emoji = payload.emoji.name if payload.emoji.is_unicode_emoji() else payload.emoji.id
+            role_id = self.emoji_to_role[emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
             return
 
         role = guild.get_role(role_id)
@@ -41,21 +44,23 @@ class RoleReactClient(discord.Client):
             # If we want to do something in case of errors we'd do it here.
             pass
 
-    async def on_raw_reaction_remove(self, payload):
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         """Removes a role based on a reaction emoji."""
         # Make sure that the message the user is reacting to is the one we care about
         if payload.message_id != self.role_message_id:
             return
 
-        try:
-            role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-            # If the emoji isn't the one we care about then exit as well.
-            return
-
         guild = self.get_guild(payload.guild_id)
         if guild is None:
             # Check if we're still in the guild and it's cached.
+            return
+
+        try:
+            # If it is a unicode emoji, it should use the name, otherwise it should use the ID.
+            emoji = payload.emoji.name if payload.emoji.is_unicode_emoji() else payload.emoji.id
+            role_id = self.emoji_to_role[emoji]
+        except KeyError:
+            # If the emoji isn't the one we care about then exit as well.
             return
 
         role = guild.get_role(role_id)
@@ -80,4 +85,4 @@ intents = discord.Intents.default()
 intents.members = True
 
 client = RoleReactClient(intents=intents)
-client.run("token")
+client.run('token')
