840bec5b09741e526bea23bd024272c268499bcd
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2021-03-04 22:23:12 -0600

    new import
    imports the Sink

--- discord/__init__.py
+++ discord/__init__.py
@@ -59,6 +59,7 @@ from .audit_logs import AuditLogChanges, AuditLogEntry, AuditLogDiff
 from .raw_models import *
 from .team import *
 from .sticker import Sticker
+from .listener import Sink
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
