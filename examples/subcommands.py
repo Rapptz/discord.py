f03f08aaf213e4cc50aa78c8e020911bfbe07ba5
Snaptraks <snaptraks@gmail.com>
2021-03-16 11:03:34 -0400

    Add intents

--- examples/subcommands.py
+++ examples/subcommands.py
@@ -1,7 +1,9 @@
+import discord
 from discord.ext import commands
 
 description = """An example bot to showcase the subcommands feature."""
-bot = commands.Bot(command_prefix='?', description=description)
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 
 # first command-subcommand group
