b42640d709788daea0ec02a30fcad4f968cd1a4e
pikaninja <mileshua06@gmail.com>
2021-04-02 09:44:19 -0700

    Fix comment wording, spelling mistakes, and replace double with single quotes

--- examples/subcommands.py
+++ examples/subcommands.py
@@ -5,7 +5,6 @@ description = '''An example bot to showcase subcommands in the discord.ext.comma
 module.
 '''
 
-# Our bot object
 bot = commands.Bot(command_prefix='?', description=description)
 
 # Our `commands.Group` object, which will be our parent command
@@ -21,22 +20,22 @@ async def greet(ctx):
         # If none are found we send our help command
         await ctx.send_help(ctx.command)
 
-# Defining our subcommand as an command of our group
+# Defining our subcommand as a command of our group
 # We will invoke this command with `?greet hello`
-# We can define additional checks here
+# We can define additional checks specific to this subcommand
 @greet.command()
 @commands.is_owner()
 async def hello(ctx):
     """Say hello!"""
     await ctx.send("Hello {0.author}!".format(ctx))
 
-# This would be invoked with `?greet goodbye @Example#0000`
+# This would be invoked with `?greet goodbye <member>`
 @greet.command()
 async def goodbye(ctx, member: discord.Member):
     """Say goodbye to someone"""
     await ctx.send("Goodbye {0.author}".format(member))
 
-# We can also do sub sub commands!
+# We can also nest groups!
 @greet.group()
 async def everyone(ctx):
     """Say hi to everyone"""
@@ -44,35 +43,35 @@ async def everyone(ctx):
     if ctx.invoked_subcommand is None:
         await ctx.send_help(ctx.command)
 
-# Define our sub sub command
+# Define our nested subcommand
 # As we defined hello above already, we can not make another one without overriding
-# To fix this, we can use the name kwarg
-# Also notice because these are seperate subcommands, we can have multiple commands with the same name
+# To fix this, we can use the name keyword argument
+# Also notice because these are separate subcommands, we can have multiple commands with the same name
 # Invoked with `?greet everyone hello`
 @everyone.command(name="hello")
 async def everyone_hello(ctx):
     """Say hi to everyone!"""
     await ctx.send("Hello everyone!")
 
-# Sometimes we want our subcommands to function indenpendently from our subcommands
-# To do this, we can use the `invoke_without_command` kwarg
-# When set to True, the parent command will only be invoked when subcommands aren't
+# Sometimes you may want a subcommand to function independently from the rest
+# To do this, we can use the `invoke_without_command` keyword argument
+# When set to `True`, the parent command will only be invoked when no subcommand is found
 # Invoked with `?role <member> <role>`
 @bot.group(invoke_without_command=True)
 @commands.has_guild_permissions(manage_roles=True)
 async def role(ctx, member: discord.Member, role: discord.Role):
     """Add a role to a target member"""
     await member.add_roles(role)
-    await ctx.send("Done!")
+    await ctx.send('Done!')
 
-# Because we made our group seperate, checks will not carry over from the parent command
+# Because we made our group separate, checks will not carry over from the parent command
 # Invoked with `?role info <member>`
 @role.command()
 async def info(ctx, member: discord.Member):
     """View the roles a member has"""
-    roles = "\n".join(r.mention for r in member.roles)
+    roles = '\n'.join(r.mention for r in member.roles)
     await ctx.send(
-        "{0} Currently has:\n {1}".format(member, roles),
+        '{0} Currently has:\n {1}'.format(member, roles),
         # Don't want to ping anyone :^)
         allowed_mentions = discord.AllowedMentions.none()
     )
