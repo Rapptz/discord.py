3e1f66019674f0afbc3ac93658b06b089481f0c2
NCPlayz <chowdhurynadir0@outlook.com>
2021-04-07 19:41:39 +0100

    referenced_message can be null

--- discord/types/message.py
+++ discord/types/message.py
@@ -111,7 +111,7 @@ class _MessageOptional(TypedDict, total=False):
     message_reference: MessageReference
     flags: int
     stickers: List[Sticker]
-    referenced_message: Message
+    referenced_message: Optional[Message]
 
 
 MessageType = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 19, 20]
