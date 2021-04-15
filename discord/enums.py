75f757199841a4ad68e3beb9c3735bcf46fecb03
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2021-04-15 15:29:48 +0200

    Update enums.py

--- discord/enums.py
+++ discord/enums.py
@@ -160,25 +160,20 @@ class ChannelType(Enum):
 
 class MessageType(Enum):
     default                                      = 0
-    recipient_add                                = 1
-    recipient_remove                             = 2
-    call                                         = 3
-    channel_name_change                          = 4
-    channel_icon_change                          = 5
-    pins_add                                     = 6
-    new_member                                   = 7
-    premium_guild_subscription                   = 8
-    premium_guild_tier_1                         = 9
-    premium_guild_tier_2                         = 10
-    premium_guild_tier_3                         = 11
-    channel_follow_add                           = 12
-    guild_stream                                 = 13
-    guild_discovery_disqualified                 = 14
-    guild_discovery_requalified                  = 15
-    guild_discovery_grace_period_initial_warning = 16
-    guild_discovery_grace_period_final_warning   = 17
-    reply                                        = 19
-    application_command                          = 20
+    pins_add                                     = 1
+    new_member                                   = 2
+    premium_guild_subscription                   = 3
+    premium_guild_tier_1                         = 4
+    premium_guild_tier_2                         = 5
+    premium_guild_tier_3                         = 6
+    channel_follow_add                           = 7
+    guild_stream                                 = 8
+    guild_discovery_disqualified                 = 9
+    guild_discovery_requalified                  = 10
+    guild_discovery_grace_period_initial_warning = 11
+    guild_discovery_grace_period_final_warning   = 12
+    reply                                        = 13
+    application_command                          = 14
 
 class VoiceRegion(Enum):
     us_west       = 'us-west'
