8f9819eb4c1c0ddd477c8ff4f80deec8aea054cc
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2021-04-16 03:41:41 +0100

    [docs] Fix various unresolved references

--- discord/embeds.py
+++ discord/embeds.py
@@ -528,7 +528,7 @@ class Embed:
 
     @property
     def fields(self) -> List[_EmbedFieldProxy]:
-        """Union[List[:class:`EmbedProxy`], :attr:`Empty`]: Returns a :class:`list` of ``EmbedProxy`` denoting the field contents.
+        """List[Union[``EmbedProxy``, :attr:`Empty`]]: Returns a :class:`list` of ``EmbedProxy`` denoting the field contents.
 
         See :meth:`add_field` for possible values you can access.
 
