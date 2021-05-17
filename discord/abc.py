da0419bbf4515e28a9da02d2e6a21be667cc2726
MrKomodoDragon <74436682+MrKomodoDragon@users.noreply.github.com>
2021-05-17 14:32:25 -0700

    Improve the example for `abc.Messageable.typing`

--- discord/abc.py
+++ discord/abc.py
@@ -1310,10 +1310,11 @@ class Messageable(Protocol):
             This means that both ``with`` and ``async with`` work with this.
 
         Example Usage: ::
+        
+            async with channel.typing(): 
+                await asyncio.sleep(10) # something heavy 
 
-            async with channel.typing():
-                # do expensive stuff here
-                await channel.send('done!')
+            await channel.send('done!')
 
         """
         return Typing(self)
