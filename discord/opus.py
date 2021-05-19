b2527965a48d5c6f5c9564472acb2f3da2214f7c
Sheepposu <49356627+Sheepposu@users.noreply.github.com>
2021-05-19 10:40:20 -0500

    Update discord/opus.py
    Co-authored-by: Bluenix <bluenixdev@gmail.com>

--- discord/opus.py
+++ discord/opus.py
@@ -413,8 +413,7 @@ class Decoder(_OpusStruct):
             channel_count = self.CHANNELS
         else:
             frames = self.packet_get_nb_frames(data)
-            # channel_count = self.packet_get_nb_channels(data)
-            channel_count = self.CHANNELS  # Original code returned 1, which caused problems
+            channel_count = self.CHANNELS
             samples_per_frame = self.packet_get_samples_per_frame(data)
             frame_size = frames * samples_per_frame
 
