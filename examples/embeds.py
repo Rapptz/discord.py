4ddd45905e1a91d6720d775fe9caf7cd76bc54ba
Daishiky <77997662+Daishiky@users.noreply.github.com>
2021-05-21 12:36:34 +0200

    Update embeds.py

--- examples/embeds.py
+++ examples/embeds.py
@@ -32,36 +32,36 @@ bot = commands.Bot(command_prefix='?', description=description, intents=intents)
   #what's the difference? thumbnail is a tiny image which will be placed on top right in the embed, image is a big image placed after all the text (title, description exc..)
   
   #now let's add a field into the embed
-    embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
+  embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
   
-    embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
-    embed.set_image(url="same thing as thumbnail).")
+  embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
+  embed.set_image(url="same thing as thumbnail).")
     
-    embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
-    #how fields works? they works exactly same as title and description, just with different names. Fields are tiny boxes into embed where you can add informations if needed, they can me max 3 x row.
+  embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
+  #how fields works? they works exactly same as title and description, just with different names. Fields are tiny boxes into embed where you can add informations if needed, they can me max 3 x row.
     
-    #well, there's also a footer, which is a tiny text after all the stuff in the embed.
-    embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
+  #well, there's also a footer, which is a tiny text after all the stuff in the embed.
+  embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
   
-    embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
-    embed.set_image(url="same thing as thumbnail).")
+  embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
+  embed.set_image(url="same thing as thumbnail).")
     
-    embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
+  embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
     
-    embed.set_footer(text="your footer text goes here", icon_url="footer takes also urls to place a tiny image before the footer text.")
-    #you can't use hyperlinks in foooter.
+  embed.set_footer(text="your footer text goes here", icon_url="footer takes also urls to place a tiny image before the footer text.")
+  #you can't use hyperlinks in foooter.
     
-    #you can also add an author on the embed
-    embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
+  #you can also add an author on the embed
+  embed = discord.Embed(title="the title of the embed", description="place the embed description here", color = "here you can use discorc colors, or HEXA colors.")
   
-    embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
-    embed.set_image(url="same thing as thumbnail).")
+  embed.set_thumbnail(url="this can be a web url (which redirect directly to the image), or an internal file, like 'thumbnail.png').")
+  embed.set_image(url="same thing as thumbnail).")
     
-    embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
+  embed.add_field(name="the title of the field goes here", value="the value of the field goes here")
     
-    embed.set_footer(text="your footer text goes here", icon_url="footer takes also urls to place a tiny image before the footer text.")
+  embed.set_footer(text="your footer text goes here", icon_url="footer takes also urls to place a tiny image before the footer text.")
     
-    embed.set_author(name="the name of the author you want, can be like the {ctx.author.name}", icon_url="the icon which needs to be displayed before the author text")
+  embed.set_author(name="the name of the author you want, can be like the {ctx.author.name}", icon_url="the icon which needs to be displayed before the author text")
     
     
     # FINAL RESULT
