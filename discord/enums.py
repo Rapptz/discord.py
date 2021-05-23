fc0a00a88f959cec909b240508d146b9bffd291d
JelNiSław <s.jelnicki@gmail.com>
2021-05-23 12:08:39 +0200

    Align level values

--- discord/enums.py
+++ discord/enums.py
@@ -225,10 +225,10 @@ class SpeakingState(Enum):
         return self.value
 
 class VerificationLevel(Enum):
-    none      = 0
-    low       = 1
-    medium    = 2
-    high      = 3
+    none    = 0
+    low     = 1
+    medium  = 2
+    high    = 3
     highest = 4
 
     def __str__(self):
