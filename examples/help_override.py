49bb762b00b203ba7a49c611bdde5c7c457a0ed7
rachit1601 <70265590+rachit1601@users.noreply.github.com>
2021-05-27 13:54:58 +0530

    Added help override example
    Added help override example.
    The new help command in embed format

---
+++ examples/help_override.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+description="A simple bot to show how to override the default help command"
+
+bot=commands.Bot(command_prefix='!',description=description)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+bot.remove_command("help")
+
+
+# Embed Help ->
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="Bot Help", description="Below is the list of commands that you can use with the bot")
+    embed.add_field(name="!ping",value="Returns pong",inline=False)
+    embed.add_field(name="!cmd1", value="Gives reply 1",inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def ping(ctx):
+    ctx.send("Pong!")
+
+@bot.command()
+async def cmd1(ctx):
+    ctx.send("reply 1)
+
+bot.run("Nzk1NTg5MzI3MTE0NjAwNTE4.X_LkTA.UByVoav6Fil8wx9syCaENdDsjLU")
+    
