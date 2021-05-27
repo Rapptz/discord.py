fbafe20e510af604f7c81e87130409ea7e4526b8
Rapptz <rapptz@gmail.com>
2021-05-27 01:41:18 -0400

    Allow View to be instantiated without subclassing

--- discord/ui/view.py
+++ discord/ui/view.py
@@ -93,9 +93,7 @@ class View:
     )
 
     __discord_ui_view__: ClassVar[bool] = True
-
-    if TYPE_CHECKING:
-        __view_children_items__: ClassVar[List[ItemCallbackType]]
+    __view_children_items__: ClassVar[List[ItemCallbackType]] = []
 
     def __init_subclass__(cls) -> None:
         children: List[ItemCallbackType] = []
