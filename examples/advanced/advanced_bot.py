db40576788fb8e988b1f1189444a85bbdd349552
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-06-14 16:52:56 +0200

    Update advanced_bot.py

--- examples/advanced/advanced_bot.py
+++ examples/advanced/advanced_bot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncpg, asyncio
 from discord.ext import commands
 from datetime import datetime
 
@@ -6,12 +6,17 @@ class MyAdvancedBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(command_prefix="example ", *args, **kwargs)
         self.start_time = datetime.utcnow()
-    
+ 
     async def on_ready(self):
-        print(f"{'*' * 20}\nLogged in!\nBot User: {self.user.name}#{self.user.discriminator}\n{'*' * 20}")
+        print(f"{'*' * 20}\nLogged in!\nBot User: {self.user}\n{'*' * 20}")
 
 bot = MyAdvancedBot()
 
+async def run():
+    bot.db = await asyncpg.create_pool("db URL")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS economy(user_id bigint, wallet bigint, bank bigint)")
+
 if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(run())
     bot.load_extension("cogs.example")
     bot.run("token")
