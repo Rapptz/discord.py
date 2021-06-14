2825f3a3c6721cae6f9ee68dd5aae667bb149ee7
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-06-14 17:03:51 +0200

    Update economy.py

--- examples/advanced/cogs/economy.py
+++ examples/advanced/cogs/economy.py
@@ -1,5 +1,3 @@
-# We highly recommend using a database, but for this example, we will use a python dict.
-
 import discord
 from discord.ext import commands
 from typing import Union
@@ -9,29 +7,26 @@ class Economy(commands.Cog):
         self.bot = bot
         self.users = {}
     
-    @commands.command(name="balance")
-    async def bal(self, ctx, user : Union[discord.Member, int]):
+    async def create_bal(self, user_id):
         user_id = user.id
         try:
-            user_bal = self.users[str(user_id)]
-        except KeyError:
-            self.users[str(user_id)] = {"wallet": 0, "bank": 0}
-        user_bal = self.users[str(user_id)]
-        embed = discord.Embed(colour=discord.Colour.green(), title=f"{user.name}'s Balance", description=f"**Wallet**: {user_bal['wallet']} Coins\n**Bank**: {user_bal['bank']} Coins")
+            self.user_bal = await self.bot.db.fetchrow("SELECT * FROM economy WHERE user_id = $1", user_id)
+        except Exception:
+            await self.bot.db.execute("INSERT INTO economy(user_id, wallet, bank) VALUES ($1, 0, 0)", user_id)
+            self.user_bal = await self.bot.db.fetchrow("SELECT * FROM economy WHERE user_id = $1", user_id)
+    
+    @commands.command(name="balance")
+    async def bal(self, ctx, user : Union[discord.Member, int]):     
+        embed = discord.Embed(colour=discord.Colour.green(), title=f"{user.name}'s Balance", description=f"**Wallet**: {self.user_bal['wallet']} Coins\n**Bank**: {self.user_bal['bank']} Coins")
         await ctx.send(embed=embed)
 
 
     @commands.command(name="beg")
     async def beg(self, ctx):
-        user_id = ctx.author.id
-        try:
-            user_bal = self.users[str(user_id)]
-        except KeyError:
-            self.users[str(user_id)] = {"wallet": 0, "bank": 0}
-        user_bal = self.users[str(user_id)]
+        await self.make_bal(user.id)
         earnings = random.randint(0, 200)
         await ctx.send(f"You got {earnings} coins!!")
-        user_bal['wallet'] += earnings
+        await self.bot.db.execute("UPDATE economy SET wallet = $1 WHERE user_id = $2", self.user_bal['wallet']+earnings, ctx.author.id)
 
 def setup(bot):
     bot.add_cog(Economy(bot))
