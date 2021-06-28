8b7e5a50b40a92034e8296ae6617822761b5c4f9
quiprr <quiprr@ametrine.dev>
2021-06-27 20:48:04 -0700

    Add discord.Spotify.track_url

--- discord/activity.py
+++ discord/activity.py
@@ -674,6 +674,14 @@ class Spotify:
     def track_id(self):
         """:class:`str`: The track ID used by Spotify to identify this song."""
         return self._sync_id
+    
+    @property
+    def track_url(self) -> str:
+        """:class:`str`: The track URL to listen on Spotify.
+        
+        .. versionadded:: 2.0
+        """
+        return f'https://open.spotify.com/track/{self.track_id}'
 
     @property
     def start(self):
