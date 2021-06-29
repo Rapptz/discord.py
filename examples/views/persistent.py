e684e95a41b5cb49720d06498ba3948c9e6af0b5
NCPlayz <chowdhurynadir0@outlook.com>
2021-06-29 21:37:21 +0100

    make it better tm

--- examples/views/persistent.py
+++ examples/views/persistent.py
@@ -2,15 +2,6 @@ from discord.ext import commands
 import discord
 
 
-class PersistentViewBot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('$'))
-
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-
 # Define a simple View that persists between bot restarts
 # In order a view to persist between restarts it needs to meet the following conditions:
 # 1) The timeout of the View has to be set to None
@@ -36,17 +27,26 @@ class PersistentView(discord.ui.View):
         await interaction.response.send_message('This is grey.', ephemeral=True)
 
 
+class PersistentViewBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or('$'))
+        self.persistent_views_added = False
+
+    async def on_ready(self):
+        if not self.persistent_views_added:
+            # Register the persistent view for listening here.
+            # Note that this does not send the view to any message.
+            # In order to do this you need to first send a message with the View, which is shown below.
+            # If you have the message_id you can also pass it as a keyword argument, but for this example
+            # we don't have one.
+            self.add_view(PersistentView())
+            self.persistent_views_added = True
+
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+
 bot = PersistentViewBot()
-
-
-@bot.event
-async def on_ready():
-    # Register the persistent view for listening in on_ready.
-    # Note that this does not send the view to any message.
-    # In order to do this you need to first send a message with the View, which is shown below.
-    # If you have the message_id you can also pass it as a keyword argument, but for this example
-    # we don't have one.
-    bot.add_view(PersistentView())
 
 
 @bot.command()
