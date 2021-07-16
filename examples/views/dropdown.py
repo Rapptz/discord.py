ffdc93751f1ce7ba9d1ef9c39840dd915750bfc1
Robin5605 <74519799+Robin5605@users.noreply.github.com>
2021-07-16 16:40:54 -0500

    Added an example for the new dropdowns

---
+++ examples/views/dropdown.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or('$'))
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+#Gives us a view containing a dropdown menu
+class Dropdown(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    #Send a message pinging the user with their selected choice
+    @discord.ui.select(options=[
+        discord.SelectOption(label="Red"), 
+        discord.SelectOption(label="Green"), 
+        discord.SelectOption(label="Blue")
+    ])
+    async def callback(self, select : discord.ui.Select, interaction : discord.Interaction):
+        await interaction.response.send_message(f"{interaction.user.mention}'s favourite color is {select.values[0]}!")
+
+bot = Bot()
+
+@bot.command()
+async def select(ctx : commands.Context):
+    """Sends a message with a dropdown."""
+
+    #Creating the view
+    view = Dropdown()
+
+    #Sending the message with the view
+    await ctx.send("Pick your favourite color:", view = view)
+
+bot.run(TOKEN)
