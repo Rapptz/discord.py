41f3998a082b90bbf6e52c2ecd0c8b27d498bf3a
Rapptz <rapptz@gmail.com>
2021-07-31 19:38:38 -0400

    Fix on_thread_member_remove not dispatching

--- discord/state.py
+++ discord/state.py
@@ -864,7 +864,7 @@ class ConnectionState:
         for member_id in removed_member_ids:
             if member_id != self_id:
                 member = thread._pop_member(member_id)
-                self.dispatch('thread_member_leave', member)
+                self.dispatch('thread_member_remove', member)
             else:
                 self.dispatch('thread_remove', thread)
 
