5892102f34bb0adddf89ecb43d2610a495cf9659
TheLeadingLlama <44980366+StockerMC@users.noreply.github.com>
2021-08-18 18:14:48 -0400

    Typehinted context_managers.py and replaced ensure_future with create_task

--- discord/context_managers.py
+++ discord/context_managers.py
@@ -22,13 +22,25 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+from __future__ import annotations
+
 import asyncio
+from typing import TYPE_CHECKING, TypeVar, Optional, Type
+
+if TYPE_CHECKING:
+    from .abc import Messageable
+
+    from asyncio import AbstractEventLoop, Future
+    from types import TracebackType
+
+    T = TypeVar('T', bound='Typing')
+    BE = TypeVar('BE', bound=BaseException)
 
 __all__ = (
     'Typing',
 )
 
-def _typing_done_callback(fut):
+def _typing_done_callback(fut: Future) -> None:
     # just retrieve any exception and call it a day
     try:
         fut.exception()
@@ -36,11 +48,11 @@ def _typing_done_callback(fut):
         pass
 
 class Typing:
-    def __init__(self, messageable):
-        self.loop = messageable._state.loop
-        self.messageable = messageable
+    def __init__(self, messageable: Messageable) -> None:
+        self.loop: AbstractEventLoop = messageable._state.loop
+        self.messageable: Messageable = messageable
 
-    async def do_typing(self):
+    async def do_typing(self) -> None:
         try:
             channel = self._channel
         except AttributeError:
@@ -52,18 +64,26 @@ class Typing:
             await typing(channel.id)
             await asyncio.sleep(5)
 
-    def __enter__(self):
-        self.task = asyncio.ensure_future(self.do_typing(), loop=self.loop)
+    def __enter__(self: T) -> T:
+        self.task: asyncio.Task = self.loop.create_task(self.do_typing())
         self.task.add_done_callback(_typing_done_callback)
         return self
 
-    def __exit__(self, exc_type, exc, tb):
+    def __exit__(self,
+        exc_type: Optional[Type[BE]],
+        exc: Optional[BE],
+        tb: Optional[TracebackType]
+    ) -> None:
         self.task.cancel()
 
-    async def __aenter__(self):
+    async def __aenter__(self: T) -> T:
         self._channel = channel = await self.messageable._get_channel()
         await channel._state.http.send_typing(channel.id)
         return self.__enter__()
 
-    async def __aexit__(self, exc_type, exc, tb):
+    async def __aexit__(self,
+        exc_type: Optional[Type[BE]],
+        exc: Optional[BE],
+        tb: Optional[TracebackType]
+    ) -> None:
         self.task.cancel()
