a8449eaa5601423dfec0b472fed4031798848b62
Josh <josh.ja.butt@gmail.com>
2021-08-19 14:28:36 +1000

    Re-define properties inferred from User on Member

--- discord/member.py
+++ discord/member.py
@@ -48,7 +48,9 @@ __all__ = (
 )
 
 if TYPE_CHECKING:
-    from .channel import VoiceChannel, StageChannel
+    from .asset import Asset
+    from .channel import DMChannel, VoiceChannel, StageChannel
+    from .flags import PublicUserFlags
     from .guild import Guild
     from .types.activity import PartialPresenceUpdate
     from .types.member import (
@@ -270,12 +272,28 @@ class Member(discord.abc.Messageable, _UserTag):
         bot: bool
         system: bool
         created_at: datetime.datetime
-        default_avatar = User.default_avatar
-        avatar = User.avatar
-        dm_channel = User.dm_channel
+
+        @property
+        def default_avatar(self) -> Asset:
+            ...
+
+        @property
+        def avatar(self) -> Asset:
+            ...
+
+        @property
+        def dm_channel(self) -> Optional[DMChannel]:
+            ...
+
         create_dm = User.create_dm
-        mutual_guilds = User.mutual_guilds
-        public_flags = User.public_flags
+
+        @property
+        def mutual_guilds(self) -> List[Guild]:
+            ...
+
+        @property
+        def public_flags(self) -> PublicUserFlags:
+            ...
 
     def __init__(self, *, data: GatewayMemberPayload, guild: Guild, state: ConnectionState):
         self._state: ConnectionState = state
