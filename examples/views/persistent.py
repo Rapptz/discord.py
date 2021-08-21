35b1a1ee33ccbfafd24d80d8f4516c2b002f71c5
HyperGH <46067571+HyperGH@users.noreply.github.com>
2021-08-21 18:41:25 +0200

    Add persistent views on every on_ready
    Add persistent views on every on_ready to avoid issues with disconnects and READY clearing state as discussed in #7437

--- examples/views/persistent.py
+++ examples/views/persistent.py
@@ -30,17 +30,14 @@ class PersistentView(discord.ui.View):
 class PersistentViewBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or('$'))
-        self.persistent_views_added = False
 
     async def on_ready(self):
-        if not self.persistent_views_added:
             # Register the persistent view for listening here.
             # Note that this does not send the view to any message.
             # In order to do this you need to first send a message with the View, which is shown below.
             # If you have the message_id you can also pass it as a keyword argument, but for this example
             # we don't have one.
             self.add_view(PersistentView())
-            self.persistent_views_added = True
 
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
