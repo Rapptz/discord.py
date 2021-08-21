7d7dfe75bac6269329cb89ae856b27e23af50ba0
HyperGH <46067571+HyperGH@users.noreply.github.com>
2021-08-21 18:42:41 +0200

    Fix indentation

--- examples/views/persistent.py
+++ examples/views/persistent.py
@@ -32,13 +32,12 @@ class PersistentViewBot(commands.Bot):
         super().__init__(command_prefix=commands.when_mentioned_or('$'))
 
     async def on_ready(self):
-            # Register the persistent view for listening here.
-            # Note that this does not send the view to any message.
-            # In order to do this you need to first send a message with the View, which is shown below.
-            # If you have the message_id you can also pass it as a keyword argument, but for this example
-            # we don't have one.
-            self.add_view(PersistentView())
-
+        # Register the persistent view for listening here.
+        # Note that this does not send the view to any message.
+        # In order to do this you need to first send a message with the View, which is shown below.
+        # If you have the message_id you can also pass it as a keyword argument, but for this example
+        # we don't have one.
+        self.add_view(PersistentView())
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
