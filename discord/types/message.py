f99b45880f4437d54dea96199334ebcd2f6b79ee
dolfies <jeyalfie47@gmail.com>
2022-02-20 13:09:25 -0500

    Fix small typing issue

--- discord/types/message.py
+++ discord/types/message.py
@@ -108,7 +108,7 @@ class _MessageOptional(TypedDict, total=False):
     components: List[Component]
 
 
-MessageType = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21]
+MessageType = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21, 22, 23]
 
 
 class Message(_MessageOptional):
