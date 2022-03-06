6d58c81ec63c5e91cb1f381e2aa26bc717f63265
Vincent <vincentbusiness55@gmail.com>
2022-03-06 11:51:49 +0800

    Add more description to example

--- examples/application_commands/slash_command_trees.py
+++ examples/application_commands/slash_command_trees.py
@@ -13,29 +13,31 @@ async def on_ready():
     # add the commands to our guild
     await apple_group.sync(guild=discord.Object(id=12345))
 
+# intiate the command on top of our coroutine
 @apple_group.command(guild=discord.Object(id=12345))
-@app_commands.describe(apple='The Apple to Pick') # describe what the command is
+@app_commands.describe(apple='The Apple to Pick') # describe what the option is
 async def pick(inter: discord.Interaction, apple: str):
-    """Pick an Apple from a tree"""
- 
+    """Pick an Apple from a tree""" # the docstring representing the commands description
+
     # make sure the apple isn't already picked
     if apple in apples_picked:
         await inter.response.send_message('This Apple is already picked!')
 
-    # pick the apple
+    # add the apple to the list
     apples_picked.append(apple)
     await inter.response.send_message(f'You picked {apple}!')
 
+# intiate the command on top of our coroutine
 @apple_group.command(guild=discord.Object(id=12345))
-@app_commands.describe(apple='The Apple to Plant')
+@app_commands.describe(apple='The Apple to Plant') # describe what the option is
 async def plant(inter: discord.Interaction, apple: str):
-    """Plant a Apple"""
+    """Plant a Apple""" # the docstring representing the commands description
 
     # make sure the apple is picked
     if apple not in apples_picked:
         await inter.response.send_message('This Apple hasn\'t been picked yet!')
 
-    # plant the apple
+    # remove the apple from the list
     apples_picked.remove(apple)
     await inter.response.send_message(f'Your planted {apple}!')
 
