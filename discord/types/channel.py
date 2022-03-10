4bc9819ef5d4be4dc9e7efadc22c358638a646e2
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-03-10 06:28:33 +0500

    Fix type for guild_scheduled_event_id key.

--- discord/types/channel.py
+++ discord/types/channel.py
@@ -156,4 +156,4 @@ class StageInstance(TypedDict):
     topic: str
     privacy_level: PrivacyLevel
     discoverable_disabled: bool
-    guild_scheduled_event_id: int
+    guild_scheduled_event_id: Optional[int]
