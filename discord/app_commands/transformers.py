9fd63ad7f3d95e38bb2a21f027bdccbf886371b0
Rapptz <rapptz@gmail.com>
2022-03-12 02:03:17 -0500

    Fix Optional type hints not respecting explicitly set defaults

--- discord/app_commands/transformers.py
+++ discord/app_commands/transformers.py
@@ -627,10 +627,11 @@ def annotation_to_parameter(annotation: Any, parameter: inspect.Parameter) -> Co
 
     (inner, default) = get_supported_annotation(annotation)
     type = inner.type()
-    if default is MISSING:
-        default = parameter.default
-        if default is parameter.empty:
-            default = MISSING
+
+    if default is MISSING or default is None:
+        param_default = parameter.default
+        if param_default is not parameter.empty:
+            default = param_default
 
     # Verify validity of the default parameter
     if default is not MISSING:
