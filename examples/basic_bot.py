beafaa8a8b8006357c2a1ca6802ed80e000f4cda
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-15 17:08:34 +0530

    Change intent comments to be consistent in the examples

--- examples/basic_bot.py
+++ examples/basic_bot.py
@@ -1,4 +1,4 @@
-# This example requires the 'members' and 'message_content' privileged intents
+# This example requires the 'members' and 'message_content' privileged intents to function.
 
 import discord
 from discord.ext import commands
