afe9355b0e9e3cde66fb5618feb68e3937ea054a
Josh <josh.ja.butt@gmail.com>
2022-03-20 00:17:46 +1000

    use _set_context in __call__

--- discord/ext/commands/help.py
+++ discord/ext/commands/help.py
@@ -271,7 +271,7 @@ class HelpCommand(HelpCommandCommand, Generic[ContextT]):
         )
 
     async def __call__(self, context: ContextT, *args: Any, **kwargs: Any) -> Any:
-        return await self.command_callback(context, *args, **kwargs)
+        return await self._set_context(context, *args, **kwargs)
 
     async def _set_context(self, context: ContextT, *args: Any, **kwargs: Any) -> Any:
         _context.set(context)
