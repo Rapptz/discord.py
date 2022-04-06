a2e9f112b08ce00ed53bbbb0c9edb681b82344ff
Willy <19799671+Willy-C@users.noreply.github.com>
2022-04-06 18:12:09 -0400

    Fix missing ctx in example

--- discord/ext/commands/parameters.py
+++ discord/ext/commands/parameters.py
@@ -195,7 +195,7 @@ def parameter(
     .. code-block:: python3
 
         @bot.command()
-        async def wave(to: discord.User = commands.parameter(default=lambda ctx: ctx.author)):
+        async def wave(ctx, to: discord.User = commands.parameter(default=lambda ctx: ctx.author)):
             await ctx.send(f'Hello {to.mention} :wave:')
 
     Parameters
