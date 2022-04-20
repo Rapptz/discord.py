354b70e80c384b5c823cf937c0221420723389df
LostLuma <lilly@lostluma.dev>
2022-04-20 17:05:01 +0200

    Remove extraneous word in docstring

--- discord/raw_models.py
+++ discord/raw_models.py
@@ -301,7 +301,7 @@ class RawThreadDeleteEvent(_RawReprMixin):
     guild_id: :class:`int`
         The ID of the guild the thread was deleted in.
     parent_id: :class:`int`
-        The ID of the channel the thread was belonged to.
+        The ID of the channel the thread belonged to.
     thread: Optional[:class:`discord.Thread`]
         The thread, if it could be found in the internal cache.
     """
