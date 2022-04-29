ba5e244c57b6ebe837fd5e1af6c51609dfac8450
Narmy <jo25bu@gmail.com>
2022-04-29 20:41:05 +0200

    Change example to use discord.Client

--- examples/app_commands/basic.py
+++ examples/app_commands/basic.py
@@ -2,12 +2,15 @@ from typing import Optional
 
 import discord
 from discord import app_commands
-from discord.ext import commands
 
 MY_GUILD = discord.Object(id=0)  # replace with your guild id
 
 
-class MyBot(commands.Bot):
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents, application_id: int):
+        super().__init__(intents=intents, application_id=application_id)
+
+        self.tree = app_commands.CommandTree(self)
 
     # In this basic example, we just synchronize the app commands to one guild,
     # without requiring to set the guild to every command individually by just copying them over.
@@ -22,22 +25,21 @@ intents = discord.Intents.default()
 
 # In order to use a basic synchronization of the app commands in the setup_hook,
 # you have replace ... with your bots application_id you find in the developer portal.
-bot = MyBot(command_prefix='?', intents=intents, application_id=...)
+client = MyClient(intents=intents, application_id=0)
 
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
     print('------')
 
 
-@bot.tree.command()
+@client.tree.command()
 async def hello(interaction: discord.Interaction):
     """Says hello!"""
     await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
 
-@bot.tree.command()
+@client.tree.command()
 @app_commands.describe(
     first_value='The first value you want to add something to', second_value='The value you want to add to the first value'
 )
@@ -47,7 +49,7 @@ async def add(interaction: discord.Interaction, first_value: int, second_value: 
 
 # To make an argument optional, you can either give it a supported default argument
 # or you can mark it as Optional from the typing library. This example does both.
-@bot.tree.command()
+@client.tree.command()
 @app_commands.describe(member='The member you want to get the joined date from, defaults to the user who uses the command')
 async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
     """Says when a member joined."""
@@ -57,4 +59,4 @@ async def joined(interaction: discord.Interaction, member: Optional[discord.Memb
     await interaction.response.send_message(f'{member} joined in {member.joined_at}')
 
 
-bot.run('token')
+client.run('token')
