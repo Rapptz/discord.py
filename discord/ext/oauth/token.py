498cf3acd986171b2b6fb0cd1fe02d26f8f20717
FrostiiWeeb <rayanhassansuja@gmail.com>
2022-04-30 13:22:28 +0200

    Add oauth2 extension.

---
+++ discord/ext/oauth/token.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Token:
+    access_token: str
+    refresh_token: str
+    token_type: str
+    expires_in: datetime
+    scope: str
+
+    def __str__(self) -> str:
+        return f"{self.access_token}"
+
+    @property
+    def expired(self) -> bool:
+        return self.expires_in >= datetime.utcnow()
