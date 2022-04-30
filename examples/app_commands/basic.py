c19ef7dea709247a9c0b4184c36b989c2f3bec06
Narmy <jo25bu@gmail.com>
2022-04-30 13:21:53 +0200

    Add basic context menu examples

--- examples/app_commands/basic.py
+++ examples/app_commands/basic.py
@@ -68,4 +68,20 @@ async def joined(interaction: discord.Interaction, member: Optional[discord.Memb
     await interaction.response.send_message(f'{member} joined in {member.joined_at}')
 
 
+# A Context Menu is an app that can be run on a member or on a message by rightclicking.
+# It always takes an interaction as its first parameter and a Member, Message or Union of both as its second parameter.
+
+# This context menu only works for members
+@client.tree.context_menu(name='Joindate')
+async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+    # We're sending this message as ephemeral, so only the command user can see it
+    await interaction.response.send_message(f'{member} joined in {member.joined_at}', ephemeral=True)
+
+
+# This context menu only works for messages
+@client.tree.context_menu(name='Wrong channel')
+async def wrong_channel(interaction: discord.Interaction, message: discord.Message):
+    await interaction.response.send_message(f'{message.author.mention} To talk about this, please move to #off-topic')
+
+
 client.run('token')
