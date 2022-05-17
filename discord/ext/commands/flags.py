49c059b3cac85729cb1f5ebf739912d98469d85d
Wayoshi <WayoshiM@users.noreply.github.com>
2022-05-17 16:41:02 -0400

    [commands] fix case_insensitive bug in parse_flags
    Co-authored-by: Danny <Rapptz@users.noreply.github.com>

--- discord/ext/commands/flags.py
+++ discord/ext/commands/flags.py
@@ -499,10 +499,12 @@ class FlagConverter(metaclass=FlagsMeta):
                 if not value:
                     raise MissingFlagArgument(last_flag)
 
+                name = last_flag.name.casefold() if case_insensitive else last_flag.name
+
                 try:
-                    values = result[last_flag.name]
+                    values = result[name]
                 except KeyError:
-                    result[last_flag.name] = [value]
+                    result[name] = [value]
                 else:
                     values.append(value)
 
@@ -515,10 +517,12 @@ class FlagConverter(metaclass=FlagsMeta):
             if not value:
                 raise MissingFlagArgument(last_flag)
 
+            name = last_flag.name.casefold() if case_insensitive else last_flag.name
+
             try:
-                values = result[last_flag.name]
+                values = result[name]
             except KeyError:
-                result[last_flag.name] = [value]
+                result[name] = [value]
             else:
                 values.append(value)
 
