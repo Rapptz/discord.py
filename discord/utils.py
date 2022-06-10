95658284881d58e0348500eaac4a0d138b30f7c9
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-06-10 12:57:41 +0530

    Update utils.py

--- discord/utils.py
+++ discord/utils.py
@@ -288,7 +288,7 @@ def oauth_url(
     redirect_uri: str = MISSING,
     scopes: Iterable[str] = MISSING,
     disable_guild_select: bool = False,
-    state: str = MISSING,
+    state: str = MISSING
 ) -> str:
     """A helper function that returns the OAuth2 URL for inviting the bot
     into guilds.
@@ -341,7 +341,7 @@ def oauth_url(
         if redirect_uri is not MISSING:
             url += '&response_type=code&' + urlencode({'redirect_uri': redirect_uri})
         if state is not MISSING:
-            url += urlencode({'state': state})
+            url += f'&{urlencode({"state": state})}'
     return url
 
 
