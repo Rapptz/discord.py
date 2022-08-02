db4cbd68ace4c2c090e366bfe354fba2d65902cb
Marenzers <marenzie@pm.me>
2022-08-02 11:29:05 -0500

    fixes:#8285 The allowed type for move_to included StageChannel in the documentation, but wasn't actually added to the coroutine.

--- discord/member.py
+++ discord/member.py
@@ -911,7 +911,9 @@ class Member(discord.abc.Messageable, _UserTag):
         else:
             await self._state.http.edit_my_voice_state(self.guild.id, payload)
 
-    async def move_to(self, channel: Optional[VocalGuildChannel], *, reason: Optional[str] = None) -> None:
+    async def move_to(
+            self, channel: Optional[Union[VocalGuildChannel, StageChannel]], *, reason: Optional[str] = None
+    ) -> None:
         """|coro|
 
         Moves a member to a new voice channel (they must be connected first).
