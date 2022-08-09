df54da8bc9e87ba75c94a73e8c577e46524bf52b
Rapptz <rapptz@gmail.com>
2022-08-09 10:42:15 -0400

    Fix secret example requiring more intents than necessary

--- examples/secret.py
+++ examples/secret.py
@@ -6,7 +6,6 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
 
 bot = commands.Bot(command_prefix=commands.when_mentioned, description="Nothing to see here!", intents=intents)
 
