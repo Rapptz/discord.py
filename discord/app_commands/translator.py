a9730a5a2f3d2a205756a3c69bae51d1d1317828
Bryan Forbes <bryan@reigndropsfall.net>
2022-08-14 19:35:25 -0500

    Update discord/app_commands/translator.py
    Co-authored-by: Danny <1695103+Rapptz@users.noreply.github.com>

--- discord/app_commands/translator.py
+++ discord/app_commands/translator.py
@@ -219,11 +219,9 @@ class Translator:
             The locale being requested for translation.
         context: :class:`TranslationContext`
             The translation context where the string originated from.
-
-            .. note::
-                For better type checking ergonomics, the ``TranslationContextTypes``
-                type can be used instead to aid with type narrowing. It is functionally
-                equivalent to :class:`TranslationContext`.
+            For better type checking ergonomics, the ``TranslationContextTypes``
+            type can be used instead to aid with type narrowing. It is functionally
+            equivalent to :class:`TranslationContext`.
         """
 
         return None
