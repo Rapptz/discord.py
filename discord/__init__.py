a3baca1e55afd204f113e6a0ca0d15ef293b8105
Varun J <root@5ht2.me>
2022-11-16 21:10:31 +0530

    Fix version string
    specify release level in `__version__`

--- discord/__init__.py
+++ discord/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'discord'
 __author__ = 'Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-present Rapptz'
-__version__ = '2.2.0'
+__version__ = '2.2.0a'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
