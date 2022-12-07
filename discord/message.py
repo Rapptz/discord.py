8ff0a0e701b74371e5ec33f755c290f0dfe6725f
Varun J <57328245+lmaotrigine@users.noreply.github.com>
2022-12-07 10:55:41 +0530

    Fix reactions with custom emojis breaking
    Discord has changed the representation of custom emoji in reaction payloads, apparently.
    This commit strips the leading colon from reaction emoji strings to comply with this change.

--- discord/message.py
+++ discord/message.py
@@ -120,9 +120,9 @@ def convert_emoji_reaction(emoji: Union[EmojiInputType, Reaction]) -> str:
     if isinstance(emoji, PartialEmoji):
         return emoji._as_reaction()
     if isinstance(emoji, str):
-        # Reactions can be in :name:id format, but not <:name:id>.
+        # Reactions can be in name:id format, but not <:name:id>.
         # No existing emojis have <> in them, so this should be okay.
-        return emoji.strip('<>')
+        return emoji.strip('<:>')
 
     raise TypeError(f'emoji argument must be str, Emoji, or Reaction not {emoji.__class__.__name__}.')
 
