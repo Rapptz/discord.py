4b5126553532d6129a8f27e0a5730369bcef69ef
itttgg <spdd5724@gmail.com>
2023-01-04 20:50:52 +0300

    Edit basic.py

--- examples/cogs/basic.py
+++ examples/cogs/basic.py
@@ -1,17 +1,15 @@
 # In this example you will learn how to make cogs (not extensions)
 
+from asyncio import sleep as async_sleep
+
 import discord
 from discord.ext import commands
-from discord.ext.commands import HelpCommand
 from discord import app_commands
-
-from typing import Optional, Type, Any
-from asyncio import sleep as async_sleep
 
 
 class MyCog(commands.Cog):
-    def __init__(self, bot) -> None:
-        self.bot = bot  # Assign bot who is setuping cog
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -25,42 +23,28 @@ class MyCog(commands.Cog):
     async def admin_check(self, ctx: commands.Context):
         await ctx.send('You are admin!')
 
-    @app_commands.command()
+    @app_commands.command(description='Test app command in cogs')
     @app_commands.describe(first='First number', second='Second number', msg='Message')
     async def test_app_command(self, interaction: discord.Interaction, first: int, second: int, msg: str):
-        """
-        Test app command in cogs
-        """
         await interaction.response.send_message('Hello, world!')
         await async_sleep(1.0)  # Sleep by 1 second
         await interaction.response.edit_message(content=f'{first} + {second} = {first + second}. {msg}')
-    
+
     @app_commands.context_menu()
-    @app_commands.default_permissions(administrator=True)  # Only admin can use this command
+    @app_commands.default_permissions(administrator=True)  # Only admins can use this command
     async def ban(self, interaction: discord.Interaction, member: discord.Member):
         await member.ban(reason='Admin wants to ban you')
+        await interaction.response.send_message(f'Banned {str(member)}')
 
 
 # Create bot with cogs
-class Bot(commands.Bot):
-    def __init__(
-        self,
-        command_prefix,
-        *,
-        help_command: Optional[HelpCommand] = ...,
-        tree_cls: Type[app_commands.CommandTree[Any]] = app_commands.CommandTree,
-        description: Optional[str] = None,
-        intents: discord.Intents,
-        **options: Any,
-    ) -> None:
-        super().__init__(
-            command_prefix=command_prefix,
-            help_command=help_command,
-            tree_cls=tree_cls,
-            description=description,
-            intents=intents,
-            **options,
-        )
+class MyBot(commands.Bot):
+    def __init__(self) -> None:
+        command_prefix = '!'
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(command_prefix, intents=intents)
 
     async def setup_hook(self):
         print('The start of cogs installation')
@@ -68,9 +52,6 @@ class Bot(commands.Bot):
         print('The end of cogs installation')
 
 
-intents = discord.Intents.default()
-intents.message_content = True
+bot = MyBot()
 
-bot = Bot(command_prefix='!', intents=intents)
-
-bot.run('YOUR_TOKEN')  # Replace with your token
+bot.run('token')  # Replace with your token
