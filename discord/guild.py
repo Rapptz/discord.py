ace46f8e05ef506db97766e810a4a60251ba3c41
Lia Marie <93722449+qt-haskell@users.noreply.github.com>
2023-01-10 22:22:51 +0100

    Update Guild.query_members docstring

--- discord/guild.py
+++ discord/guild.py
@@ -4099,9 +4099,7 @@ class Guild(Hashable):
     ) -> List[Member]:
         """|coro|
 
-        Request members that belong to this guild whose username starts with
-        the query given.
-
+        Request members of this guild whose username or nickname starts with the given query.
         This is a websocket operation.
 
         .. note::
@@ -4116,7 +4114,7 @@ class Guild(Hashable):
         Parameters
         -----------
         query: Optional[:class:`str`]
-            The string that the username's start with.
+            The string that the username or nickname should start with.
         limit: :class:`int`
             The maximum number of members to send back. This must be
             a number between 5 and 100.
