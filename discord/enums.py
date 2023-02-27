bd6bbdab007e0ac8b21fbdae9c91f2d1e981eaa2
Rapptz <rapptz@gmail.com>
2023-02-27 09:17:40 -0500

    Add support for stage message types

--- discord/enums.py
+++ discord/enums.py
@@ -234,11 +234,11 @@ class MessageType(Enum):
     auto_moderation_action = 24
     role_subscription_purchase = 25
     interaction_premium_upsell = 26
-    # stage_start = 27
-    # stage_end = 28
-    # stage_speaker = 29
-    # stage_raise_hand = 30
-    # stage_topic = 31
+    stage_start = 27
+    stage_end = 28
+    stage_speaker = 29
+    stage_raise_hand = 30
+    stage_topic = 31
     guild_application_premium_subscription = 32
 
 
