1de3562f3475ee29ddcbfd51351586710ec953db
Bryan Forbes <bryan@reigndropsfall.net>
2023-03-01 18:14:50 -0600

    Fix partially uknown typing errors

--- discord/client.py
+++ discord/client.py
@@ -1161,9 +1161,9 @@ class Client:
         event: Literal['app_command_completion'],
         /,
         *,
-        check: Optional[Callable[[Interaction[Self], Union[Command, ContextMenu]], bool]],
+        check: Optional[Callable[[Interaction[Self], Union[Command[Any, Any, Any], ContextMenu]], bool]],
         timeout: Optional[float] = None,
-    ) -> Tuple[Interaction[Self], Union[Command, ContextMenu]]:
+    ) -> Tuple[Interaction[Self], Union[Command[Any, Any, Any], ContextMenu]]:
         ...
 
     # AutoMod
@@ -1816,9 +1816,9 @@ class Client:
         event: Literal["command", "command_completion"],
         /,
         *,
-        check: Optional[Callable[[Context], bool]] = None,
+        check: Optional[Callable[[Context[Any]], bool]] = None,
         timeout: Optional[float] = None,
-    ) -> Context:
+    ) -> Context[Any]:
         ...
 
     @overload
@@ -1827,9 +1827,9 @@ class Client:
         event: Literal["command_error"],
         /,
         *,
-        check: Optional[Callable[[Context, CommandError], bool]] = None,
+        check: Optional[Callable[[Context[Any], CommandError], bool]] = None,
         timeout: Optional[float] = None,
-    ) -> Tuple[Context, CommandError]:
+    ) -> Tuple[Context[Any], CommandError]:
         ...
 
     @overload
