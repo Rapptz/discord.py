f4cca624de4dd1e8aa1ddfe4a6ad3c40f494a4d8
Puncher <65789180+Puncher1@users.noreply.github.com>
2023-03-25 23:00:03 +0100

    Update `all_channel()`

--- discord/permissions.py
+++ discord/permissions.py
@@ -221,8 +221,11 @@ class Permissions(BaseFlags):
            Added :attr:`create_public_threads`, :attr:`create_private_threads`, :attr:`manage_threads`,
            :attr:`use_external_stickers`, :attr:`send_messages_in_threads` and
            :attr:`request_to_speak` permissions.
+
+        .. versionchanged:: 2.3
+           Added :attr:`use_soundboard`
         """
-        return cls(0b111110110110011111101111111111101010001)
+        return cls(0b1000111110110110011111101111111111101010001)
 
     @classmethod
     def general(cls) -> Self:
