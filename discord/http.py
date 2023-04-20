23add00e923b19baf123520dcfb20cd23c7e36e6
Puncher <65789180+Puncher1@users.noreply.github.com>
2023-04-20 16:50:41 +0200

    with_counts defaults to True + fix lints

--- discord/http.py
+++ discord/http.py
@@ -1373,14 +1373,10 @@ class HTTPClient:
         limit: int,
         before: Optional[Snowflake] = None,
         after: Optional[Snowflake] = None,
-        with_counts: bool = MISSING,
+        with_counts: bool = True,
     ) -> Response[List[guild.Guild]]:
-        params: Dict[str, Any] = {
-            'limit': limit,
-        }
+        params: Dict[str, Any] = {'limit': limit, 'with_counts': int(with_counts)}
 
-        if with_counts is not MISSING:
-            params['with_counts'] = int(with_counts)
         if before:
             params['before'] = before
         if after:
