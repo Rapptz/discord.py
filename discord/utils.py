c57d488ca699de69a240ae7c8304bd53da23141d
Rapptz <rapptz@gmail.com>
2023-04-25 00:19:20 -0400

    Fix FileHandler having ANSI escapes when used within PyCharm

--- discord/utils.py
+++ discord/utils.py
@@ -1243,11 +1243,12 @@ def is_docker() -> bool:
 
 
 def stream_supports_colour(stream: Any) -> bool:
+    is_a_tty = hasattr(stream, 'isatty') and stream.isatty()
+
     # Pycharm and Vscode support colour in their inbuilt editors
     if 'PYCHARM_HOSTED' in os.environ or os.environ.get('TERM_PROGRAM') == 'vscode':
-        return True
+        return is_a_tty
 
-    is_a_tty = hasattr(stream, 'isatty') and stream.isatty()
     if sys.platform != 'win32':
         # Docker does not consistently have a tty attached to it
         return is_a_tty or is_docker()
