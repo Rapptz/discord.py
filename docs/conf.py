6e2c1777d1845ab52f1fa472dbc205cd9a76c857
Alex Liu <30390986+applebee1558@users.noreply.github.com>
2023-05-09 22:48:41 -0700

    fix sphinx build issue

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ autodoc_typehints = 'none'
 # napoleon_attr_annotations = False
 
 extlinks = {
-    'issue': ('https://github.com/Rapptz/discord.py/issues/%s', 'GH-'),
+    'issue': ('https://github.com/Rapptz/discord.py/issues/%s', 'GH-%s'),
     'ddocs': ('https://discord.com/developers/docs/%s', None),
 }
 
