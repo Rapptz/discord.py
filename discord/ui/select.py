5de91412820c995f98448922a3f43ab7f070e860
yvqc <88755906+yvqc@users.noreply.github.com>
2023-05-31 03:34:31 +0200

    Better TypeError message when setting custom_id
    None is not allowed as a value, the error message is now clear.

--- discord/ui/select.py
+++ discord/ui/select.py
@@ -162,10 +162,10 @@ class BaseSelect(Item[V]):
     @custom_id.setter
     def custom_id(self, value: str) -> None:
         if not isinstance(value, str):
-            raise TypeError('custom_id must be None or str')
+            raise TypeError('custom_id must be a str')
 
         self._underlying.custom_id = value
-        self._provided_custom_id = value is not None
+        self._provided_custom_id = True
 
     @property
     def placeholder(self) -> Optional[str]:
