b3c7959e0fa70e2819ab7128c15b79f967b4cfe0
Gobot1234 <gobot1234yt@gmail.com>
2023-09-14 19:53:14 +0100

    Add inline-tabs

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ extras_require = {
         'sphinxcontrib_trio==1.1.2',
         'sphinxcontrib-websupport',
         'typing-extensions>=4.3,<5',
+        'sphinx-inline-tabs',
     ],
     'speed': [
         'orjson>=3.5.4',
