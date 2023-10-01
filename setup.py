f758ba8c36c5372427088ca79e587f603e4b9789
mc_fdc <Masato.04.11.2007@gmail.com>
2023-10-01 12:01:58 +0900

    Support 3.12 too

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
