ffd0ce2020dafe3f6f4dede53d109f7ce843cbe2
mc_fdc <Masato.04.11.2007@gmail.com>
2023-10-01 12:49:04 +0900

    Remove 3.12

--- setup.py
+++ setup.py
@@ -96,7 +96,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
