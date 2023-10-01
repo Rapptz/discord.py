ebfa48b15272f2ae017ca022b507d1cf7585d203
mc_fdc <Masato.04.11.2007@gmail.com>
2023-10-01 12:05:28 +0900

    Add 3.11 and 3.12

--- setup.py
+++ setup.py
@@ -95,6 +95,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
