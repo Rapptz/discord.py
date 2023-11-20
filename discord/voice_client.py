12b3430a802fd6369d1e86598e763f2bdd66f008
Danny <1695103+Rapptz@users.noreply.github.com>
2023-11-19 19:11:37 -0500

    Update discord/voice_client.py

--- discord/voice_client.py
+++ discord/voice_client.py
@@ -110,7 +110,8 @@ class VoiceProtocol:
         data: :class:`dict`
             The raw :ddocs:`voice state payload <resources/voice#voice-state-object>`.
 
-        .. warning ::
+        .. warning::
+
             This method is not the same as the event. See: :func:`on_voice_state_update`
         """
         raise NotImplementedError
