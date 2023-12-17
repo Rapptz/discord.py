cf97a1d1f800d82e28005dbbb2ad1843fd0b7de7
Ed Powers <57187688+edpowers@users.noreply.github.com>
2023-12-17 03:06:11 -0300

    Fix fallback build number retrieval again (#628)

--- discord/utils.py
+++ discord/utils.py
@@ -1477,7 +1477,7 @@ async def _get_build_number(session: ClientSession) -> int:  # Thank you Discord
     try:
         login_page_request = await session.get('https://discord.com/login', timeout=7)
         login_page = await login_page_request.text()
-        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)\.js').findall(login_page)[-2] + '.js'
+        build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z.0-9]+)\.js').findall(login_page)[-2] + '.js'
         build_request = await session.get(build_url, timeout=7)
         build_file = await build_request.text()
         build_find = re.findall(r'Build Number:\D+"(\d+)"', build_file)
