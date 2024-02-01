cff6aa1ea09647a76682c5ec6334645e5e9b5986
Alex Nørgaard <umbra@abstractumbra.dev>
2024-02-01 12:37:48 +0000

    client.py -> mutated Self and assigned Self generic to _get_state return

--- discord/client.py
+++ discord/client.py
@@ -285,7 +285,7 @@ class Client:
         }
 
         self._enable_debug_events: bool = options.pop('enable_debug_events', False)
-        self._connection: ConnectionState[Self] = self._get_state(intents=intents, **options)
+        self._connection: ConnectionState[Client] = self._get_state(intents=intents, **options)
         self._connection.shard_count = self.shard_count
         self._closed: bool = False
         self._ready: asyncio.Event = MISSING
@@ -315,7 +315,7 @@ class Client:
     def _get_websocket(self, guild_id: Optional[int] = None, *, shard_id: Optional[int] = None) -> DiscordWebSocket:
         return self.ws
 
-    def _get_state(self, **options: Any) -> ConnectionState:
+    def _get_state(self, **options: Any) -> ConnectionState[Self]:
         return ConnectionState(dispatch=self.dispatch, handlers=self._handlers, hooks=self._hooks, http=self.http, **options)
 
     def _handle_ready(self) -> None:
