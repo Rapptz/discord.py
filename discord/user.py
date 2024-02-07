4404ab8a88e0dd21868e0a12bc76458322a6411d
JDJG <ghostzap123@gmail.com>
2024-02-07 18:33:41 -0500

    May be formatted better

--- discord/user.py
+++ discord/user.py
@@ -426,7 +426,7 @@ class ClientUser(BaseUser):
             A :term:`py:bytes-like object` representing the image to upload.
             Could be ``None`` to denote no avatar.
 
-        The only image formats supported for uploading is JPEG, PNG, GIF AND WEBP.
+        The only image formats supported for uploading is JPEG, PNG, GIF, WEBP
 
         Raises
         ------
