4cbaa4f61a984b3a5e8a35636c9a963034f919b8
JDJG <ghostzap123@gmail.com>
2024-02-07 18:38:42 -0500

    Let me try this.

--- discord/user.py
+++ discord/user.py
@@ -425,8 +425,7 @@ class ClientUser(BaseUser):
         avatar: Optional[:class:`bytes`]
             A :term:`py:bytes-like object` representing the image to upload.
             Could be ``None`` to denote no avatar.
-
-        The only image formats supported for uploading is JPEG, PNG, GIF, WEBP
+            The only image formats supported for uploading is JPEG, PNG, GIF, WEBP
 
         Raises
         ------
