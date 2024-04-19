59cf5f3f9737dfd06aa78fda79d52c52a8d4e946
Timo <154911483+timoo4devv@users.noreply.github.com>
2024-04-19 17:53:31 +0200

    Add slashcommand cog example

---
+++ examples/cog_basic.py
@@ -0,0 +1,23 @@
+import discord
+
+from discord.ext import commands
+from discord import app_commands
+
+class Cog_basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command()
+    async def roll(self, ctx: discord.Interaction, dice: str):
+        """Rolls a dice in NdN format."""
+        try:
+            rolls, limit = map(int, dice.split('d'))
+        except Exception:
+            await ctx.response.send_message('Format has to be in NdN!')
+            return
+
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        await ctx.response.send_message(result)
+
+async def setup(bot):
+    await bot.add_cog(Cog_basic(bot))
