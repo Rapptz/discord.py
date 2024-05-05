d964851c5f5b757506d32801095b1a73602492f2
Developer Anonymous <kibandhonor@gmail.com>
2024-05-05 12:11:01 +0200

    remove poll checks

--- discord/http.py
+++ discord/http.py
@@ -260,10 +260,6 @@ def handle_message_parameters(
         payload.update(channel_payload)
 
     if poll not in (MISSING, None):
-        if len(poll) == 0 or len(poll) > 10:
-            raise ValueError('Poll must contain between 1 and 10 answers')
-        if poll._hours_duration < 1 or poll._hours_duration > 168:
-            raise ValueError('Polls duration must be between 1 hour and 7 days')
         payload['poll'] = poll._to_dict()  # type: ignore
 
     multipart = []
