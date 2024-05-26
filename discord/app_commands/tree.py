3fd4c957ff4a71925ed318e8b9d08a900c7cfedf
Soheab_ <33902984+Soheab@users.noreply.github.com>
2024-05-26 19:16:04 +0200

    Add notes

--- discord/app_commands/tree.py
+++ discord/app_commands/tree.py
@@ -877,10 +877,24 @@ class CommandTree(Generic[ClientT]):
         guild: Optional[:class:`~discord.abc.Snowflake`]
             The guild to add the command to. If not given or ``None`` then it
             becomes a global command instead.
+
+            .. note ::
+
+                Due to a Discord limitation, this kwarg cannot be used in conjunction with 
+                contexts (e.g. :func:`.app_commands.allowed_contexts`) or installation types
+                (e.g. :func:`.app_commands.allowed_installs`).
+
         guilds: List[:class:`~discord.abc.Snowflake`]
             The list of guilds to add the command to. This cannot be mixed
             with the ``guild`` parameter. If no guilds are given at all
             then it becomes a global command instead.
+
+            .. note ::
+
+                Due to a Discord limitation, this kwarg cannot be used in conjunction with 
+                contexts (e.g. :func:`.app_commands.allowed_contexts`) or installation types
+                (e.g. :func:`.app_commands.allowed_installs`).
+
         auto_locale_strings: :class:`bool`
             If this is set to ``True``, then all translatable strings will implicitly
             be wrapped into :class:`locale_str` rather than :class:`str`. This could
@@ -960,10 +974,24 @@ class CommandTree(Generic[ClientT]):
         guild: Optional[:class:`~discord.abc.Snowflake`]
             The guild to add the command to. If not given or ``None`` then it
             becomes a global command instead.
+
+            .. note ::
+
+                Due to a Discord limitation, this kwarg cannot be used in conjunction with 
+                contexts (e.g. :func:`.app_commands.allowed_contexts`) or installation types
+                (e.g. :func:`.app_commands.allowed_installs`).
+
         guilds: List[:class:`~discord.abc.Snowflake`]
             The list of guilds to add the command to. This cannot be mixed
             with the ``guild`` parameter. If no guilds are given at all
             then it becomes a global command instead.
+
+            .. note ::
+
+                Due to a Discord limitation, this kwarg cannot be used in conjunction with 
+                contexts (e.g. :func:`.app_commands.allowed_contexts`) or installation types
+                (e.g. :func:`.app_commands.allowed_installs`).
+
         auto_locale_strings: :class:`bool`
             If this is set to ``True``, then all translatable strings will implicitly
             be wrapped into :class:`locale_str` rather than :class:`str`. This could
