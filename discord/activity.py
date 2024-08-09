8bc04d53b13f80e51fc2eb0ee596152d3c000beb
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-08-09 18:45:19 +0200

    Change how data is handled
    even you can't set it (yet)

--- discord/activity.py
+++ discord/activity.py
@@ -865,13 +865,23 @@ class HangStatus(BaseActivity):
 
     __slots__ = ('state', 'name', 'emoji')
 
-    def __init__(self, state: str, emoji: Optional[Union[PartialEmoji, Dict[str, Any], str]] = None, **extra: Any) -> None:
+    def __init__(
+        self,
+        state: HangStatusType,
+        *,
+        name: Optional[str] = None,
+        emoji: Optional[Union[PartialEmoji, Dict[str, Any], str]] = None,
+        **extra: Any,
+    ) -> None:
         super().__init__(**extra)
-        self.state: HangStatusType = try_enum(HangStatusType, state)
+        self.state: HangStatusType = state
 
         self.name: str
-        if self.state == HangStatusType.custom:
-            self.name = extra['details']
+        if state == HangStatusType.custom:
+            if name is None:
+                raise ValueError(f'name must be set if state is custom')
+            else:
+                self.name = extra.pop('details', name)
         else:
             self.name = self.state.value
 
@@ -968,7 +978,8 @@ def create_activity(data: Optional[ActivityPayload], state: ConnectionState) -> 
     elif game_type is ActivityType.listening and 'sync_id' in data and 'session_id' in data:
         return Spotify(**data)
     elif game_type is ActivityType.hang:
-        return HangStatus(**data)  # type: ignore
+        hang_state = try_enum(HangStatusType, data.pop('state'))
+        return HangStatus(state=hang_state, **data)  # type: ignore
     else:
         ret = Activity(**data)
 
