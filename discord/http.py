69bd72a1160795188e6283185a3c739492abca94
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-08-28 21:54:34 +0200

    Add Guild.fetch_role

--- discord/http.py
+++ discord/http.py
@@ -2455,6 +2455,9 @@ class HTTPClient:
     def get_roles(self, guild_id: Snowflake) -> Response[List[role.Role]]:
         return self.request(Route('GET', '/guilds/{guild_id}/roles', guild_id=guild_id))
 
+    def get_role(self, guild_id: Snowflake, role_id: Snowflake) -> Response[role.Role]:
+        return self.request(Route('GET', '/guilds/{guild_id}/roles/{role_id}', guild_id=guild_id, role_id=role_id))
+
     def edit_role(
         self, guild_id: Snowflake, role_id: Snowflake, *, reason: Optional[str] = None, **fields: Any
     ) -> Response[role.Role]:
