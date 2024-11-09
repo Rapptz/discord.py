7db879b5bd7008a28613cf107f434a18b8ea7303
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2024-11-09 12:20:01 +0100

    Clear up add_roles and remove_roles documentation
    Using "member" here can mislead a reader into believing this
    restriction is referring to the member being edited rather than the
    client/bot that is executing the edit.

--- discord/member.py
+++ discord/member.py
@@ -1076,7 +1076,7 @@ class Member(discord.abc.Messageable, _UserTag):
 
         You must have :attr:`~Permissions.manage_roles` to
         use this, and the added :class:`Role`\s must appear lower in the list
-        of roles than the highest role of the member.
+        of roles than the highest role of the client.
 
         Parameters
         -----------
@@ -1115,7 +1115,7 @@ class Member(discord.abc.Messageable, _UserTag):
 
         You must have :attr:`~Permissions.manage_roles` to
         use this, and the removed :class:`Role`\s must appear lower in the list
-        of roles than the highest role of the member.
+        of roles than the highest role of the client.
 
         Parameters
         -----------
