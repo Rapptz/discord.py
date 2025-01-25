1a9cbce6982ced22e178df9a76ed911c602fe752
iyad-f <128908811+iyad-f@users.noreply.github.com>
2025-01-26 01:11:12 +0530

    run black

--- discord/audit_logs.py
+++ discord/audit_logs.py
@@ -285,9 +285,11 @@ class AuditLogDiff:
 
     if TYPE_CHECKING:
 
-        def __getattr__(self, item: str) -> Any: ...
+        def __getattr__(self, item: str) -> Any:
+            ...
 
-        def __setattr__(self, key: str, value: Any) -> Any: ...
+        def __setattr__(self, key: str, value: Any) -> Any:
+            ...
 
 
 Transformer = Callable[["AuditLogEntry", Any], Any]
