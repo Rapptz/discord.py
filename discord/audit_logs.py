bca7f15588f30a6f9ac9bab89651e4f3c17f2889
ec <earkz0@gmail.com>
2025-07-20 15:58:55 -0700

    Revert "check color since discord sends that key instead of colour"
    This reverts commit 619f023448fc4bf35913956fe8e1c327c8afe53a.

--- discord/audit_logs.py
+++ discord/audit_logs.py
@@ -444,9 +444,9 @@ class AuditLogChanges:
             setattr(self.after, attr, after)
 
         # add an alias
-        if hasattr(self.after, 'color'):
+        if hasattr(self.after, 'colour'):
             self.after.color = self.after.colour
-        if hasattr(self.before, 'color'):
+        if hasattr(self.before, 'colour'):
             self.before.color = self.before.colour
         if hasattr(self.after, 'expire_behavior'):
             self.after.expire_behaviour = self.after.expire_behavior
