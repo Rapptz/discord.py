93ac32839d17916b07b9cc475c1a111f77dd4193
Soheab_ <33902984+Soheab@users.noreply.github.com>
2025-08-28 18:57:05 +0200

    Tuple[str] -> Tuple[str, ...] for aliases

--- discord/ext/commands/core.py
+++ discord/ext/commands/core.py
@@ -449,7 +449,7 @@ class Command(_BaseCommand, Generic[CogT, P, T]):
         self.brief: Optional[str] = kwargs.get('brief')
         self.usage: Optional[str] = kwargs.get('usage')
         self.rest_is_raw: bool = kwargs.get('rest_is_raw', False)
-        self.aliases: Union[List[str], Tuple[str]] = kwargs.get('aliases', [])
+        self.aliases: Union[List[str], Tuple[str, ...]] = kwargs.get('aliases', [])
         self.extras: Dict[Any, Any] = kwargs.get('extras', {})
 
         if not isinstance(self.aliases, (list, tuple)):
