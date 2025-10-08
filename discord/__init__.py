0115942c78e2824c2c6536f59e147c56f6fc5eb1
Rapptz <rapptz@gmail.com>
2025-10-08 17:43:16 -0400

    Version bump to v2.6.4

--- discord/__init__.py
+++ discord/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'discord'
 __author__ = 'Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-present Rapptz'
-__version__ = '2.6.3'
+__version__ = '2.6.4'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
@@ -85,7 +85,7 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=2, minor=6, micro=3, releaselevel='final', serial=0)
+version_info: VersionInfo = VersionInfo(major=2, minor=6, micro=4, releaselevel='final', serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
