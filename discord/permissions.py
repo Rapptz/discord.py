31c0276f5bc6dd16a6bff1d6178207cbddd681d6
Sacul0457Deve <183588943+Sacul0457@users.noreply.github.com.>
2025-11-16 09:21:57 +0800

    add version changed for all_channel

--- discord/permissions.py
+++ discord/permissions.py
@@ -328,6 +328,9 @@ class Permissions(BaseFlags):
         .. versionchanged:: 2.4
             Added :attr:`send_polls`, :attr:`send_voice_messages`, attr:`use_external_sounds`,
             :attr:`use_embedded_activities`, and :attr:`use_external_apps` permissions.
+
+        .. versionchanged:: 2.7
+            Added :attr:`pin_messages` and :attr:`bypass_slowmode` permissions.
         """
         return cls(0b0000_0000_0001_1110_0110_0100_1111_1101_1011_0011_1111_0111_1111_1111_0101_0001)
 
