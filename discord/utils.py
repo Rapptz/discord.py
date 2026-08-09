b2a2df0099b935c8f858a32721ff123e29133c05
Sacul0457Deve <183588943+Sacul0457@users.noreply.github.com.>
2026-08-09 13:15:03 +0800

    fix lint: remove unused import

--- discord/utils.py
+++ discord/utils.py
@@ -57,7 +57,6 @@ from typing import (
     TYPE_CHECKING,
 )
 import unicodedata
-import collections.abc
 from itertools import islice
 from base64 import b64encode, b64decode
 from bisect import bisect_left
